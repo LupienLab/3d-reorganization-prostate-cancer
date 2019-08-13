fcb8d7321db1fa277ddfb00a3ec6307c22d7fdee
James Hawley <jamesrichardhawley@gmail.com>
2019-08-13 16:00:35 -0400

    Fixed --rotate option in script to display contact matrix as upper triangle

--- Results/2019-07-08_TADs/plot-tads.py
+++ Results/2019-07-08_TADs/plot-tads.py
@@ -67,7 +67,47 @@ def tads_to_patches(tads):
     tad_patches = [patches.PathPatch(p, edgecolor=c, facecolor='none', lw=3) for p, c in zip(paths, colours)]
     return tad_patches
 
-def show(cool_uri, range, balanced=True, out=None, dpi=300, scale='log10', force=False, zmin=None, zmax=None, cmap='YlOrRd', field='count', tads=None, verbose=None):
+def show(
+    cool_uri, range, balanced=True, out=None, dpi=300, scale='log10',
+    force=False, zmin=None, zmax=None, cmap='YlOrRd', field='count',
+    tads=None, rotate=False,
+    verbose=None
+):
+    '''
+    Plot contact matrix
+
+    Parameters
+    ----------
+    cool_uri : str
+        URI to Cooler file to plot
+    range : str
+        UCSC-compatible range to plot (chr:start-end)
+    balanced : bool
+        Plot balanced or raw contact matrix
+    out : str
+        Output image file name
+    dpi : int
+        Output image resolution
+    scale : str
+        Plotting transform function. Must be one of ['linear', 'log2', 'log10'].
+    force : bool
+        Force plotting despite memory conerns
+    zmin : float
+        Min value that colour scale covers
+    zmax : float
+        Max value that colour scale covers
+    cmap : str
+        Colour map
+    field : str
+        Column in Cooler file table to plot
+    tads : str
+        Path to TSV file containing TAD calls
+    rotate : bool
+        Rotate plot by 45 degrees
+    verbose : int
+        Verbosity level
+    '''
+    # import matplotlib package here for faster loading
     try:
         import matplotlib as mpl
         if out is not None:
@@ -75,14 +115,24 @@ def show(cool_uri, range, balanced=True, out=None, dpi=300, scale='log10', force
         import matplotlib.pyplot as plt
         from matplotlib.path import Path
         import matplotlib.patches as patches
+        import matplotlib.transforms as mtransforms
     except ImportError:
-        print("Install matplotlib to use cooler show", file=sys.stderr)
-        sys.exit(1)
+        raise ImportError("Install matplotlib to use `cooler show`")
+    # types of scaling before plotting
+    scale_types = {
+        'linear': 'relative contact frequency',
+        'log2'  : 'log 2 ( relative contact frequency )',
+        'log10' : 'log 10 ( relative contact frequency )'
+    }
+    if scale not in scale_types:
+        raise ValueError("Invalid scale. Expected one of: %s" % scale_types.dict_keys())
 
+    # load contact matri
     if verbose is not None:
         print('Loading contact matrix')
     c = Cooler(cool_uri)
 
+    # parse regions specified to plot
     chromsizes = c.chromsizes
     row_region = range
     col_region = range
@@ -91,9 +141,7 @@ def show(cool_uri, range, balanced=True, out=None, dpi=300, scale='log10', force
 
     if verbose is not None:
         print('Plotting contact matrix')
-    fig, ax = plt.subplots(figsize=(11, 10))
-    # ax.gcf().canvas.set_window_title('Contact matrix'.format())
-    # ax.title('')
+    fig, ax = plt.subplots(figsize=(5, 4))
     im = ax.imshow(
         load_matrix(c, row_region, col_region, field, balanced, scale),
         interpolation='none',
@@ -113,14 +161,35 @@ def show(cool_uri, range, balanced=True, out=None, dpi=300, scale='log10', force
         for p in tad_patches:
             ax.add_patch(p)
 
+            
+    if rotate:
+        shift_coords = ax.transData.transform([col_hi, row_lo])
+        x1, x2, y1, y2 = im.get_extent()
+        print(x1, x2, y1, y2)
+        print(col_lo, col_hi, row_hi, row_lo)
+        tr = (
+            mtransforms.Affine2D()
+            .rotate_deg(-45)
+            .scale(1 / np.sqrt(2))
+            .translate(0, row_hi)
+            # .rotate_deg_around(coords[0], coords[1], 5)
+            # .rotate_deg_around(0, 0, 5)
+        ) + ax.transData
+        # hide axes
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        # hide y axis labels and ticks
+        ax.axes.get_yaxis().set_visible(False)
+    else:
+        tr = ax.transData
+    im.set_transform(tr)
+
     # If plotting into a file, plot and quit
     plt.ylabel('{} coordinate'.format(row_chrom))
     plt.xlabel('{} coordinate'.format(col_chrom))
     cb = fig.colorbar(im, cmap=cmap)
-    cb.set_label(
-        {'linear': 'relative contact frequency',
-         'log2'  : 'log 2 ( relative contact frequency )',
-         'log10' : 'log 10 ( relative contact frequency )'}[scale])
+    cb.set_label(scale_types[scale])
     fig.savefig(out, dpi=dpi)
 
 if __name__ == '__main__':
@@ -143,6 +212,11 @@ if __name__ == '__main__':
         help='TSV containing information about TADs'
     )
     PARSER.add_argument(
+        '-r' ,'--rotate',
+        action='store_true',
+        help='Rotate contact matrix by 45 degrees in plot'
+    )
+    PARSER.add_argument(
         '-o', '--output',
         type=str,
         help='Path to output image'
@@ -156,6 +230,9 @@ if __name__ == '__main__':
     ARGS = PARSER.parse_args()
 
     # plot region
-    show(ARGS.cool, ARGS.range, balanced=True, out=ARGS.output, dpi=300, tads=ARGS.tads, verbose=ARGS.verbose)
+    show(
+        ARGS.cool, ARGS.range, balanced=True, out=ARGS.output, dpi=300,
+        tads=ARGS.tads, rotate=ARGS.rotate, verbose=ARGS.verbose
+    )
 
 
