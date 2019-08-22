7b3f9351f58cdecd8bba016a001c415c2eba061f
James Hawley <jamesrichardhawley@gmail.com>
2019-08-22 11:13:16 -0400

    Adding aggregated HiCUP reports for all samples, aggregated over batches

--- Data/Processed/2019-06-18_PCa-LowC-sequencing/agg-hicup-reports.py
+++ Data/Processed/2019-06-18_PCa-LowC-sequencing/agg-hicup-reports.py
@@ -18,23 +18,67 @@ import pandas as pd
 # Constants
 # ==============================================================================
 AGG_COLS = [
-    'File', 'Total_Reads_1', 'Total_Reads_2',
-    'Not_Truncated_Reads_1', 'Not_Truncated_Reads_2',
-    'Truncated_Read_1', 'Truncated_Read_2',
-    'Average_Length_Truncated_1', 'Average_Length_Truncated_2',
-    'Too_Short_To_Map_Read_1', 'Too_Short_To_Map_Read_2',
-    'Unique_Alignments_Read_1', 'Unique_Alignments_Read_2',
-    'Multiple_Alignments_Read_1', 'Multiple_Alignments_Read_2',
-    'Failed_To_Align_Read_1', 'Failed_To_Align_Read_2',
-    'Paired_Read_1', 'Paired_Read_2',
-    'Valid_Pairs', 'Invalid_Pairs', 'Same_Circularised', 'Same_Dangling_Ends',
-    'Same_Fragment_Internal', 'Re_Ligation', 'Contiguous_Sequence',
+    'File',
+    'Total_Reads_1',
+    'Total_Reads_2',
+    'Not_Truncated_Reads_1',
+    'Not_Truncated_Reads_2',
+    'Truncated_Read_1',
+    'Truncated_Read_2',
+    'Average_Length_Truncated_1',
+    'Average_Length_Truncated_2',
+    'Too_Short_To_Map_Read_1',
+    'Too_Short_To_Map_Read_2',
+    'Unique_Alignments_Read_1',
+    'Unique_Alignments_Read_2',
+    'Multiple_Alignments_Read_1',
+    'Multiple_Alignments_Read_2',
+    'Failed_To_Align_Read_1',
+    'Failed_To_Align_Read_2',
+    'Paired_Read_1',
+    'Paired_Read_2',
+    'Valid_Pairs',
+    'Invalid_Pairs',
+    'Same_Circularised',
+    'Same_Dangling_Ends',
+    'Same_Fragment_Internal',
+    'Re_Ligation',
+    'Contiguous_Sequence',
     'Wrong_Size',
-    'Deduplication_Read_Pairs_Uniques', 'Deduplication_Cis_Close_Uniques',
-    'Deduplication_Cis_Far_Uniques', 'Deduplication_Trans_Uniques',
-    'Percentage_Mapped', 'Percentage_Valid', 'Percentage_Uniques',
-    'Percentage_Unique_Trans', 'Percentage_Ditags_Passed_Through_HiCUP'
+    'Deduplication_Read_Pairs_Uniques',
+    'Deduplication_Cis_Close_Uniques',
+    'Deduplication_Cis_Far_Uniques',
+    'Deduplication_Trans_Uniques',
+    'Percentage_Mapped',
+    'Percentage_Valid',
+    'Percentage_Uniques',
+    'Percentage_Unique_Trans',
+    'Percentage_Ditags_Passed_Through_HiCUP'
 ]
+
+BATCHES = ['190509_A00827_0007_AHKYH3DSXX', '190605_A00827_0009_BHLGJ3DSXX']
+
+# ==============================================================================
+# Functions
+# ==============================================================================
+
+
+def read_batch_tsv(batch):
+    '''
+    Simple wrapper for pd.read_csv
+
+    Parameters
+    ----------
+    batch : str
+        Sequencing batch data folder
+    '''
+    raw_data_dir = path.join('..', '..', 'Raw')
+    report_dir = 'Reports'
+    summary_name = 'HiCUP_summary_report.tsv'
+    return pd.read_csv(
+        path.join(raw_data_dir, batch, report_dir, summary_name),
+        sep='\t', index_col=False
+    )
 
 # ==============================================================================
 # Main
@@ -48,107 +92,52 @@ def main(cfg, outdir):
     # load sample metadata
     metadata = pd.read_csv(cfg, sep='\t', index_col=False)
     samples = metadata.Sample.tolist()
-    # load summary files
-    truncation = pd.read_csv(
-        path.join('..', '..', 'Raw', '190509_A00827_0007_AHKYH3DSXX',
-                  'Reports', 'hicup_truncater_summary.tsv'),
-        sep='\t', index_col=False
+    frames = [read_batch_tsv(b) for b in BATCHES]
+    agg_data = pd.concat(frames)
+    # add sample column to table for aggregation
+    agg_data['Sample'] = None
+    for s in samples:
+        agg_data.loc[agg_data.File.str.startswith(s), 'Sample'] = s
+    # aggregate by sample, add all count columns
+    full_data = agg_data.loc[:, ~(agg_data.columns.str.startswith(
+        'Percentage') | agg_data.columns.str.startswith('Average'))].groupby('Sample').agg('sum')
+
+    # calculate percentages (ignoring the average lengths, not necessary)
+    full_data['Percentage_Mapped'] = np.round(
+        100 * (full_data['Paired_Read_1'] + full_data['Paired_Read_2']) / (
+            full_data['Total_Reads_1'] + full_data['Total_Reads_2']),
+        decimals=2
     )
-    mapping = pd.read_csv(
-        path.join('Reports', 'hicup_mapper_summary.tsv'),
-        sep='\t', index_col=False
+    full_data['Percentage_Valid'] = np.round(
+        100 * full_data['Valid_Pairs'] /
+        (full_data['Valid_Pairs'] + full_data['Invalid_Pairs']),
+        decimals=2
     )
-    filtering = pd.read_csv(
-        path.join('Reports', 'hicup_filter_summary.tsv'),
-        sep='\t', index_col=False
+    full_data['Percentage_Uniques'] = np.round(
+        100 * full_data['Deduplication_Read_Pairs_Uniques'] /
+        full_data['Valid_Pairs'],
+        decimals=2
     )
-    dedup = pd.read_csv(
-        path.join('Reports', 'hicup_deduplicator_summary.tsv'),
-        sep='\t', index_col=False
+    full_data['Percentage_Unique_Trans'] = np.round(
+        100 * full_data['Deduplication_Trans_Uniques'] /
+        full_data['Deduplication_Read_Pairs_Uniques'],
+        decimals=2
     )
-    # aggregated table
-    agg_data = pd.DataFrame(columns=AGG_COLS)
-    agg_data.loc[:, 'File'] = samples
-    # iterate through samples and extract meaningful results
-    for i, r in agg_data.iterrows():
-        s = r.at['File']
-        # extract sample-specific data from aggregate tables
-        sample_trunc = truncation.loc[truncation.File.str.startswith(s)]
-        sample_map = mapping.loc[mapping.File.str.startswith(s)]
-        sample_filter = filtering.loc[filtering.File.str.startswith(s)]
-        sample_dedup = dedup.loc[dedup.File.str.startswith(s)]
-        # assign information to agg_data
-        # what are the column names in the truncation file
-        individual_trunc_cols = [
-            'Total_Reads_Processed', 'Truncated', 'Not_truncated',
-            'Average_length_truncated_sequence'
-        ]
-        # what are the relevant names in the aggregated file
-        pair_trunc_cols = [
-            'Total_Reads', 'Truncated_Read', 'Not_Truncated_Reads',
-            'Average_Length_Truncated'
-        ]
-        # bring column data together for truncation data
-        for indiv, pair in zip(individual_trunc_cols, pair_trunc_cols):
-            r[pair + '_1'] = sample_trunc.loc[:, indiv].values[0]
-            r[pair + '_2'] = sample_trunc.loc[:, indiv].values[1]
-        # what are the column names in the mapping file
-        individual_map_cols = [
-            'Reads_too_short_to_map',
-            'Unique_alignments', 'Multiple_alignments', 'Failed_to_align',
-            'Paired'
-        ]
-        # what are the relevant names in the aggregated file
-        pair_map_cols = [
-            'Too_Short_To_Map_Read', 'Unique_Alignments_Read',
-            'Multiple_Alignments_Read', 'Failed_To_Align_Read', 'Paired_Read'
-        ]
-        # bring column data together for mapping data
-        for indiv, pair in zip(individual_map_cols, pair_map_cols):
-            r[pair + '_1'] = sample_map.loc[:, indiv].values[0]
-            r[pair + '_2'] = sample_map.loc[:, indiv].values[1]
-        # aggregate filtering data
-        individual_filt_cols = [
-            'Valid_pairs', 'Invalid_pairs', 'Same_circularised',
-            'Same_dangling_ends', 'Same_internal', 'Re-ligation',
-            'Contiguous_sequence', 'Wrong_size'
-        ]
-        agg_filt_cols = [
-            'Valid_Pairs', 'Invalid_Pairs', 'Same_Circularised',
-            'Same_Dangling_Ends', 'Same_Fragment_Internal', 'Re_Ligation',
-            'Contiguous_Sequence', 'Wrong_Size'
-        ]
-        for indiv, pair in zip(individual_filt_cols, agg_filt_cols):
-            r[pair] = sample_filter.loc[:, indiv].values[0]
-        # aggregate deduplication data
-        individual_dedup_cols = [
-            'Unique_di-tags', 'Cis_<10kbp_of_uniques', 'Cis_>10kbp_of_uniques',
-            'Trans_of_uniques'
-        ]
-        agg_dedup_cols = [
-            'Deduplication_Read_Pairs_Uniques',
-            'Deduplication_Cis_Close_Uniques', 'Deduplication_Cis_Far_Uniques',
-            'Deduplication_Trans_Uniques'
-        ]
-        for indiv, pair in zip(individual_dedup_cols, agg_dedup_cols):
-            r[pair] = sample_dedup.loc[:, indiv].values[0]
-        # calculate percentages based on column values
-        r['Percentage_Mapped'] = np.round(100 * (r['Paired_Read_1'] + r['Paired_Read_2']) / (
-            r['Total_Reads_1'] + r['Total_Reads_2']), decimals=2)
-        r['Percentage_Valid'] = np.round(
-            100 * r['Valid_Pairs'] / (r['Valid_Pairs'] + r['Invalid_Pairs']), decimals=2)
-        r['Percentage_Uniques'] = np.round(
-            100 * r['Deduplication_Read_Pairs_Uniques'] / r['Valid_Pairs'], decimals=2)
-        r['Percentage_Unique_Trans'] = np.round(
-            100 * r['Deduplication_Trans_Uniques'] / r['Deduplication_Read_Pairs_Uniques'], decimals=2)
-        r['Percentage_Ditags_Passed_Through_HiCUP'] = np.round(
-            200 * r['Deduplication_Read_Pairs_Uniques'] / (r['Total_Reads_1'] + r['Total_Reads_2']), decimals=2)
+    full_data['Percentage_Ditags_Passed_Through_HiCUP'] = np.round(
+        200 * full_data['Deduplication_Read_Pairs_Uniques'] /
+        (full_data['Total_Reads_1'] + full_data['Total_Reads_2']),
+        decimals=2
+    )
+    # rename `Sample` to `File` to match HiCUP report format
+    full_data.reset_index(drop=False, inplace=True)
+    full_data.columns.values[0] = 'File'
     # save data to output directory
-    agg_data.to_csv(
-        path.join(outdir, 'HiCUP_summary_report.tsv'), index=False, sep='\t'
+    full_data.to_csv(
+        path.join(outdir, 'HiCUP_summary_report.tsv'),
+        index=False, sep='\t', header=True
     )
-    for i, r in agg_data.iterrows():
-        agg_data.loc[[i]].to_csv(
+    for i, r in full_data.iterrows():
+        full_data.loc[[i]].to_csv(
             path.join(outdir, 'HiCUP_summary_report_{}.txt'.format(r.File)),
             index=False, sep='\t', header=True
         )
