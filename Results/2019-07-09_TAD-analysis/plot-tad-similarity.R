da2d6058991bed4ef63df7923942b1e89162bbe1
James Hawley <jamesrichardhawley@gmail.com>
2019-11-07 15:45:08 -0500

    Removing TAD comparisons from TADs folder
    This work is being moved into `2019-07-09_TAD-analysis/`

---
+++ Results/2019-07-09_TAD-analysis/plot-tad-similarity.R
@@ -0,0 +1,42 @@
+# ==============================================================================
+# Environment
+# ==============================================================================
+suppressMessages(library("data.table"))
+suppressMessages(library("ggplot2"))
+suppressMessages(library("grid"))
+
+CHRS = paste0("chr", c(1:22, "X", "Y"))
+CHRS = factor(CHRS, levels = CHRS, ordered = TRUE)
+
+# ==============================================================================
+# Data
+# ==============================================================================
+# read minimum pairwise symmetric similarity
+mpss = fread("TAD-comparisons/w_30.40000bp.similarity.tsv")
+mpss[, chr_nat := factor(chr, levels = CHRS)]
+
+# ==============================================================================
+# Plots
+# ==============================================================================
+gg = (
+    ggplot(data = mpss)
+    + geom_segment(aes(x = start, y = similarity, xend = end, yend = similarity))
+    + labs(x = "Position", y = "Min. Sym. Pairwise Similarity")
+    + facet_grid(. ~ chr_nat, switch = "x", scales = "free_x", space = "free")
+    + theme_minimal()
+    + theme(
+        panel.spacing = unit(0, "lines"),
+        panel.border = element_rect(size = 0.1, colour = "black", fill = NA),
+        strip.text.x = element_text(angle=90, hjust=0.5, vjust=0.5),
+        axis.text.x = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank()
+    )
+)
+
+ggsave(
+    "Plots/tad-similarity-track.png",
+    height = 12,
+    width = 20,
+    units = "cm"
+)
