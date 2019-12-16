954735a7a640cbebdead58ac3ea1e98b89c45438
= <=>
2019-12-16 12:30:57 -0500

    Adding plot for samples and SV count totals

--- Results/2019-07-24_breakfinder/Scripts/plot-sv-counts.R
+++ Results/2019-07-24_breakfinder/Scripts/plot-sv-counts.R
@@ -3,6 +3,9 @@
 # ==============================================================================
 suppressMessages(library("data.table"))
 suppressMessages(library("ggplot2"))
+suppressMessages(library("grid"))
+suppressMessages(library("gridExtra"))
+suppressMessages(library("gtable"))
 
 # ==============================================================================
 # Data
@@ -46,19 +49,60 @@ breakpoints = fread(
 # ==============================================================================
 # Analysis
 # ==============================================================================
+# count total number of breakpoints
 breakpoints_counted = breakpoints[, .N, by = "Sample"][order(-N)]
+colnames(breakpoints_counted) = c("PCaID", "Breakpoints")
+
+# metadata grid for plotting
+metadata_wide = melt(
+    metadata,
+    id.vars = "PCaID",
+    measure.vars = c("Gleason Score", "BCR", "ETS consensus"),
+    variable.name = "Feature",
+    value.name = "Value"
+)
+# order by sample in the same order as breakpoints_counted
+metadata_wide[, PCaID := factor(PCaID, levels = breakpoints_counted$PCaID, ordered = TRUE)]
+metadata_wide = metadata_wide[order(PCaID)]
 
 # ==============================================================================
 # Plots
 # ==============================================================================
-gg = (
+# plot according to number of SVs
+gg_bars = (
     ggplot(data = breakpoints_counted)
-    + geom_col(aes(x = Sample, y = N))
+    + geom_col(aes(x = factor(PCaID, ordered = TRUE, levels = PCaID), y = Breakpoints, fill = ))
+    + labs(x = NULL, y = "SVs Detected")
     + theme_minimal()
+    + theme(
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        panel.grid.major.x = element_blank()
+    )
 )
-ggsave(
-    "Plots/counted-breakpoints.png",
-    width = 20,
-    height = 12,
-    units = "cm"
+
+# heatmap with sample metadata
+gg_meta = (
+    ggplot(data = metadata_wide)
+    + geom_tile(aes(x = PCaID, y = Feature, fill = Value))
+    + labs(x = "Sample", y = "Feature")
+    + guides(fill = FALSE)
+    + theme_minimal()
+    + theme(
+        axis.text.x = element_text(angle = 90),
+        panel.grid.major = element_blank()
+
+        # axis.text.y = element_blank()
+    )
 )
+
+# combine plots
+bars = ggplotGrob(gg_bars)
+features = ggplotGrob(gg_meta)
+g = rbind(bars, features, size = "first")
+g$widths = unit.pmax(bars$widths, features$widths)
+
+png("Plots/counted-breakpoints.png", width = 20, height = 12, units = "cm", res = 300)
+grid.draw(g)
+dev.off()
+dev.off()
