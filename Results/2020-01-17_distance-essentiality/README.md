48c56dc00f51a857ece772e1d4ad79f18da90e66
James Hawley <jamesrichardhawley@gmail.com>
2020-01-20 18:53:11 -0500

    updating distance essentiality plot script

--- Results/2020-01-17_distance-essentiality/README.md
+++ Results/2020-01-17_distance-essentiality/README.md
@@ -3,7 +3,7 @@
 Upon earlier inspection of our data, we noticed that the _FOXA1_, _MYC_, and _AR_ genes were all located in close proximity to TAD boundaries in all 13 patients.
 The _FOXA1_ locus is shown below.
 
-![_FOXA1_ locus](../2019-10-24_higlass/foxa1.png)
+![_FOXA1_ locus](../2019-10-24_higlass/Plots/FOXA1-locus.png)
 
 These observations aligned with previous literature stating that housekeeping genes were also more frequently located near TAD boundaries.
 These observations led us to the hypothesis that essential genes for the cell of interest are located near TAD boundaries (i.e. TADs form around these essential genes).
@@ -16,5 +16,9 @@ We use gene essentiality data from the [DepMap Project](https://depmap.org) for 
 We use the aggregated TAD boundaries from [`../2020-01-15_TAD-aggregation/`](../2020-01-15_TAD-aggregation/) from primary prostate cancer tumours.
 
 ## Results
+
+### Genes are uniformly distribution across TADs
+
+![qq-plot of gene TSS location across TADs](Plots/PCa3023.proximity.qqplot.png)
 
 ## Conclusions
--- Results/2020-01-17_distance-essentiality/plot-essentiality-distance.R
+++ Results/2020-01-17_distance-essentiality/plot-essentiality-distance.R
@@ -47,18 +47,59 @@ for (cell in cells) {
     genes_melted[idx, Quintile := cut(Essentiality, breaks = breaks, labels = 1:5)]
 }
 
+callout_names = c("AR", "ETV1", "FOXA1", "HOXB13", "MYC", "SOX9")
+callouts = genes[name %in% callout_names]
+callouts_melted = genes_melted[name %in% callout_names]
+
 # ==============================================================================
 # Plots
 # ==============================================================================
 gg = (
     ggplot(data = genes[complete.cases(LNCaP)])
+    + geom_point(aes(x = Fraction, y = LNCaP), alpha = 0.1)
     + geom_smooth(aes(x = Fraction, y = LNCaP), method = "loess")
-    + labs(x = "TSS distance from boundary", y = "Essentiality in LNCaP")
-    + xlim(c(0, 0.5))
+    + labs(y = "Essentiality in LNCaP")
+    + annotate(
+        geom = "text",
+        x = callouts[, Fraction],
+        y = callouts[, LNCaP],
+        label = callouts[, name]
+    )
+    + scale_x_continuous(
+        limits = c(0, 0.5),
+        breaks = 0:5 / 10,
+        name = "TSS distance from boundary",
+        labels = paste(0:5 * 10, "%")
+    )
     + theme_minimal()
 )
 ggsave(
     paste0(ARGS$prefix, ".proximity.png"),
+    height = 12,
+    width = 20,
+    units = "cm"
+)
+
+gg = (
+    ggplot(data = genes[complete.cases(LNCaP)])
+    + geom_density2d(aes(x = Fraction, y = LNCaP))
+    + labs(y = "Essentiality in LNCaP")
+    # + annotate(
+    #     geom = "text",
+    #     x = genes[name %in% c("FOXA1", "MYC", "AR"), Fraction],
+    #     y = genes[name %in% c("FOXA1", "MYC", "AR"), LNCaP],
+    #     label = c("FOXA1", "MYC", "AR")
+    # )
+    + scale_x_continuous(
+        limits = c(0, 0.5),
+        breaks = 0:5 / 10,
+        name = "TSS distance from boundary",
+        labels = paste(0:5 * 10, "%")
+    )
+    + theme_minimal()
+)
+ggsave(
+    paste0(ARGS$prefix, ".proximity-density.png"),
     height = 12,
     width = 20,
     units = "cm"
@@ -67,13 +108,29 @@ ggsave(
 # plot essentiality across the TAD
 gg = (
     ggplot(data = genes_melted[complete.cases(genes_melted)])
+    + geom_point(aes(x = Fraction, y = Essentiality), alpha = 0.1)
     + geom_smooth(aes(x = Fraction, y = Essentiality), method = "loess")
     + labs(x = "TSS distance from boundary", y = "Essentiality")
+    + geom_text(
+        data = callouts_melted,
+        mapping = aes(
+            x = Fraction,
+            y = Essentiality,
+            label = name
+        ),
+        inherit.aes = FALSE
+    )
+    + scale_x_continuous(
+        limits = c(0, 0.5),
+        breaks = 0:5 / 10,
+        name = "TSS distance from boundary",
+        labels = paste(0:5 * 10, "%")
+    )
     + facet_wrap(~ Cell)
     + theme_minimal()
 )
 ggsave(
-    paste0(ARGS$prefix, ".proximity.png"),
+    paste0(ARGS$prefix, ".proximity.all-lines.png"),
     height = 12,
     width = 20,
     units = "cm"
@@ -82,14 +139,45 @@ ggsave(
 # for a given quintile of essentiality, what distribution do they have across a TAD?
 gg = (
     ggplot(data = genes_melted[complete.cases(genes_melted)])
-    + geom_density(aes(x = Fraction, fill = Quintile, colour = Quintile), alpha = 0)
+    # + geom_density(aes(x = Fraction, fill = Quintile, colour = Quintile), alpha = 0)
+    + geom_histogram(aes(x = Fraction), bins = 50)
     + labs(x = "TSS distance from boundary", y = "Essentiality")
-    + xlim(c(0, 0.5))
+    + scale_x_continuous(
+        limits = c(0, 0.5),
+        breaks = 0:5 / 10,
+        name = "TSS distance from boundary",
+        labels = paste(0:5 * 10, "%")
+    )
     + facet_wrap(~ Cell)
     + theme_minimal()
 )
 ggsave(
     paste0(ARGS$prefix, ".proximity-by-essentiality-quintile.png"),
+    height = 12,
+    width = 20,
+    units = "cm"
+)
+
+## qq plot of gene distribution throughout a TAD vs uniform distribution
+gg = (
+    ggplot(
+        data = genes,
+        mapping = aes(sample = Fraction)
+    )
+    + geom_qq(distribution = qunif, alpha = 0.1)
+    + scale_x_continuous(
+        limits = c(0, 1),
+        name = "Uniform distribution quantile"
+    )
+    + scale_y_continuous(
+        limits = c(0, 0.5),
+        breaks = 0:5 / 10,
+        name = "TSS distance from boundary quantile"
+    )
+    + theme_minimal()
+)
+ggsave(
+    paste0(ARGS$prefix, ".proximity.qqplot.png"),
     height = 12,
     width = 20,
     units = "cm"
