44955df5b3ec99263f0bec5cbebcd09cea9fab64
James Hawley <jamesrichardhawley@gmail.com>
2020-01-30 17:48:00 -0500

    Slightly more efficient intersection of TADs

--- Results/2019-07-09_TAD-analysis/calc-tad-overlap.sh
+++ Results/2019-07-09_TAD-analysis/calc-tad-overlap.sh
@@ -1,10 +1,26 @@
-for w in {3..30}; do
+#!/bin/bash
+
+SAMPLE_IDS=($(eval echo "PCa{13266,13848,14121,19121,3023,33173,40507,51852,53687,56413,57054,57294,58215}"))
+
+for ((w=3; w < 30; w++));
+do
     echo "${w}"
-    for s1 in PCa{3023,13266,13848,14121,19121,33173,40507,51852,53687,56413,57054,57294,58215}; do
+    for ((i=0; i < 13; i++));
+    do
+        s1=${SAMPLE_IDS[$i]}
         echo -e "\t${s1}"
-        for s2 in PCa{3023,13266,13848,14121,19121,33173,40507,51852,53687,56413,57054,57294,58215}; do
+        for ((j=i + 1; j < 13; j++));
+        do
+            s2=${SAMPLE_IDS[$j]}
             echo -e "\t\t${s2}"
-            bedtools intersect -f 0.6 -r -a ../2019-07-08_TADs/TADs/w_${w}/${s1}.40000bp.domains.bed -b ../2019-07-08_TADs/TADs/w_${w}/${s2}.40000bp.domains.bed > TAD-comparisons/w_${w}.${s1}.${s2}.intersected.bed
+            # only take TADs whos window size parameter corresponds to the current w being looped over
+            bedtools intersect \
+                -f 0.6 \
+                -r \
+                -a <(awk -v w=${w} '{if ($6 == w) print}' ../2020-01-15_TAD-aggregation/resolved-TADs/${s1}.40000bp.aggregated-domains.sorted.bedGraph) \
+                -b <(awk -v w=${w} '{if ($6 == w) print}' ../2020-01-15_TAD-aggregation/resolved-TADs/${s2}.40000bp.aggregated-domains.sorted.bedGraph) \
+                > TAD-comparisons/w_${w}.${s1}.${s2}.intersected.bed
+            cp TAD-comparisons/w_${w}.${s1}.${s2}.intersected.bed TAD-comparisons/w_${w}.${s2}.${s1}.intersected.bed
         done
     done
 done
