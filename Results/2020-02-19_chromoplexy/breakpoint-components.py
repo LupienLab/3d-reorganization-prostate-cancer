fc0549aa7ebc3d607b35f596f4214fd5091c971b
James Hawley <jamesrichardhawley@gmail.com>
2020-02-26 15:38:11 -0500

    Creating a graph for all samples, too
    Some SV breakpoints are shared between samples.
    To make identifying which samples do or don't share a breakpoint, I'm making an additional graph that contains all the samples and a "recurrent" edge to link them

--- Results/2020-02-19_chromoplexy/breakpoint-components.py
+++ Results/2020-02-19_chromoplexy/breakpoint-components.py
@@ -9,11 +9,14 @@ import os.path as path
 from numpy import unique
 import pandas as pd
 import networkx as nx
-import matplotlib.pyplot as plt
 from tqdm import tqdm
 import pickle
-
 from genomic_interval import GenomicInterval, overlapping
+
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+plt.rcParams["figure.figsize"] = (20/2.54, 20/2.54) # 20x20 cm
 
 # ==============================================================================
 # Constants
@@ -75,10 +78,11 @@ tol = 100000
 
 # create graph
 print("Creating graphs of breakpoints")
-G = {s: nx.Graph() for s in SAMPLES}
+G_all = nx.Graph()
+G_sample = {s: nx.Graph() for s in SAMPLES}
 
 # add each detected breakpoint as a node to the graph
-for s in tqdm(SAMPLES):
+for s in tqdm(SAMPLES, unit="sample"):
     patient_bps = breakpoints.loc[s]
     for bp in patient_bps.itertuples():
         # create hashable objects to store in each node
@@ -88,55 +92,91 @@ for s in tqdm(SAMPLES):
                 bp.chr_x,
                 bp.start_x,
                 bp.end_x,
-                {"notes": bp.notes, "strand": bp.strand_x},
+                {"sample": s, "notes": bp.notes, "strand": bp.strand_x},
             ),
             GenomicInterval(
                 bp.chr_y,
                 bp.start_y,
                 bp.end_y,
-                {"notes": bp.notes, "strand": bp.strand_y},
+                {"sample": s, "notes": bp.notes, "strand": bp.strand_y},
             ),
         ]
         # create nodes in the graph
         for i in intvls:
-            G[s].add_node(i)
+            G_all.add_node(i)
+            G_sample[s].add_node(i)
         # link these two nodes since they are linked breakpoints
-        G[s].add_edge(intvls[0], intvls[1], annotation=bp.annotation)
-    # connect 2 nodes if their intervals are within 100 kbp of each other
-    for i, n in enumerate(G[s]):
-        for j, m in enumerate(G[s]):
-            if i == j:
+        G_all.add_edge(intvls[0], intvls[1], annotation=bp.annotation)
+        G_sample[s].add_edge(intvls[0], intvls[1], annotation=bp.annotation)
+
+# connect 2 nodes if their intervals are within 100 kbp of each other
+for n in tqdm(G_all, position=0):
+    for m in G_all:
+        if n == m:
+            continue
+        # connect these nodes if the identified loci are within 100 kbp of each other
+        if overlapping(n, m, tol / 2):
+            # if the two breakpoints are from the same sample, they're nearby
+            if n.data["sample"] == m.data["sample"]:
+                G_all.add_edge(n, m, annotation="nearby")
+            # if the breakpoints are not from the same sample, this site is recurrent
+            else:
+                G_all.add_edge(n, m, annotation="recurrent")
+
+for s in SAMPLES:
+    for n in tqdm(G_sample[s], position=0):
+        for m in G_sample[s]:
+            if n == m:
                 continue
             # connect these nodes if the identified loci are within 100 kbp of each other
             if overlapping(n, m, tol / 2):
-                G[s].add_edge(n, m, annotation="nearby")
+                # if the two breakpoints are from the same sample, they're nearby
+                G_sample[s].add_edge(n, m, annotation="nearby")
 
 # ==============================================================================
 # Plots
 # ==============================================================================
 print("Plotting graphs")
 # colours for edges between breakpoints
-unique_annots = list(unique(breakpoints["annotation"].tolist() + ["nearby"]))
+unique_annots = list(unique(breakpoints["annotation"].tolist() + ["nearby", "recurrent"]))
 
-for s in tqdm(SAMPLES):
-    edges, annots = zip(*nx.get_edge_attributes(G[s], "annotation").items())
-    colours = [plt.cm.tab10(unique_annots.index(a)) for a in annots]
-    pos = nx.spring_layout(G[s])
+edges, e_annots = zip(*nx.get_edge_attributes(G_all, "annotation").items())
+n_colours = [plt.cm.tab20b(SAMPLES.index(n.data["sample"])) for n in G_all]
+e_colours = [plt.cm.tab10(unique_annots.index(a)) for a in e_annots]
+pos = nx.circular_layout(G_all)
+nx.draw(
+    G_all,
+    pos,
+    edgelist=None,
+    node_color=n_colours,
+    edge_color=e_colours,
+    with_labels=False,
+    width=1,
+)
+plt.savefig(path.join("Plots", "breakpoint-graph.png"), dpi=96)
+plt.close()
+
+for i, s in enumerate(SAMPLES):
+    n_colours = [plt.cm.tab20b(i)] * len(G_sample[s])
+    edges, e_annots = zip(*nx.get_edge_attributes(G_sample[s], "annotation").items())
+    e_colours = [plt.cm.tab10(unique_annots.index(a)) for a in e_annots]
+    pos = nx.spring_layout(G_sample[s])
     nx.draw(
-        G[s],
+        G_sample[s],
         pos,
-        edgelist=edges,
-        edge_color=colours,
+        edgelist=None,
+        node_color=n_colours,
+        edge_color=e_colours,
         with_labels=False,
-        # font_weight="bold",
         width=10,
     )
-    plt.savefig(path.join("Plots", s + ".png"))
+    plt.savefig(path.join("Plots", s + ".png"), dpi=96)
     plt.close()
 
 # ==============================================================================
 # Save data
 # ==============================================================================
 print("Saving graphs")
-pickle.dump(G, open("breakpoint-graphs.p", "wb"))
+pickle.dump(G_all, open("breakpoints.all-samples.p", "wb"))
+pickle.dump(G_sample, open("breakpoints.per-sample.p", "wb"))
 print("Done")
