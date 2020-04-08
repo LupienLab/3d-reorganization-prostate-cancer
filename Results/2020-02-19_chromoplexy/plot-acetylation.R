40865cc136388c24ec5a169ce88b3d947f9fc1e2
James Hawley <jamesrichardhawley@gmail.com>
2020-04-07 23:41:32 -0400

    Commiting figures for differentialy acetylation
    These are, sadly, all terrible.
    I need to rework this entier section of analyses

--- Results/2020-02-19_chromoplexy/plot-acetylation.R
+++ Results/2020-02-19_chromoplexy/plot-acetylation.R
@@ -15,9 +15,10 @@ metadata <- fread(
     sep = "\t",
     header = TRUE
 )
-SAMPLES <- paste0("PCa", metadata[, get("Sample ID")])
+metadata[, SampleID := paste0("PCa", get("Sample ID"))]
 metadata[, ChIP_file := paste0("../../Data/Processed/2019-05-03_PCa-H3K27ac-peaks/BAMs/Pca", get("Sample ID"), "_H3K27ac.sorted.dedup.bam")]
 metadata[, Ctrl_file := paste0("../../Data/Processed/2019-05-03_PCa-H3K27ac-peaks/BAMs/Pca", get("Sample ID"), "_input.sorted.dedup.bam")]
+SAMPLES <- metadata[, SampleID]
 
 # load raw counts of H3K27ac pull down and input, then take differences and get total ChIP library sizes
 library_sizes <- sapply(SAMPLES, function(s) 0)
@@ -35,20 +36,54 @@ for (s in SAMPLES) {
         col.names = c("chr", "start", "end", "count", "supported", "width", "frac_supported")
     )
     # calculate the linear scaling based on library size
+}
+library_sizes <- sapply(SAMPLES, function(s) 0)
+for (s in SAMPLES) {
+    dt_chip <- fread(
+        file.path("Acetylation", paste0(s, "_H3K27ac.induced-region-counts.bed")),
+        sep = "\t",
+        header = FALSE,
+        col.names = c("chr", "start", "end", "count", "supported", "width", "frac_supported")
+    )
+    dt_input <- fread(
+        file.path("Acetylation", paste0(s, "_input.induced-region-counts.bed")),
+        sep = "\t",
+        header = FALSE,
+        col.names = c("chr", "start", "end", "count", "supported", "width", "frac_supported")
+    )
+    # calculate the linear scaling based on library size
     library_sizes[s] <- min(dt_chip[, sum(count)], dt_input[, sum(count)])
 }
-
+ 
 # load acetylation correlations
 sample_corrs <- as.matrix(fread("acetylation-correlation.tsv", sep = "\t", header = TRUE))
 rownames(sample_corrs) <- SAMPLES
 
 # load TAD acetylation hypothesis test values
 acetyl <- fread(
-    "sv-disruption-tests.acetylation.tsv",
+    "Graphs/sv-disruption-tests.acetylation.tsv",
     sep = "\t",
     header = TRUE
 )
 
+tests <- fread(
+    "Graphs/sv-disruption-tests.tsv",
+    sep = "\t",
+    header = TRUE
+)
+
+test_group_acetyl <- rbindlist(lapply(
+    tests[, test_ID],
+    function(tid) {
+        dt <- fread(
+            paste0("Acetylation/Tests/test_", tid, ".results.tsv"),
+            sep = "\t",
+            header = TRUE
+        )
+        dt[, test_ID := tid]
+        return(dt)
+    }
+))
 # ==============================================================================
 # Plots
 # ==============================================================================
@@ -70,7 +105,7 @@ pheatmap(
     filename = "Plots/H3K27ac-correlation-tad-induced.png"
 )
 
-gg = (
+gg_qq = (
     ggplot(data = acetyl[order(p)])
     + geom_point(aes(x = -log10(ppoints(acetyl[, .N])), y = -log10(p)))
     + geom_abline(aes(intercept = 0, slope = 1), linetype = "dashed")
@@ -81,27 +116,43 @@ gg = (
     + theme_minimal()
 )
 ggsave(
-    "Plots/sv-disruption.acetylation.qq-plot.png",
+    "Plots/sv-disruption/acetylation.qq-plot.png",
+    gg_qq,
+    height = 12,
+    width = 20,
+    units = "cm"
+)
+ggsave(
+    "Plots/sv-disruption/acetylation.qq-plot.pdf",
+    gg_qq,
     height = 12,
     width = 20,
     units = "cm"
 )
 
-gg = (
+gg_pval_hist = (
     ggplot(data = acetyl)
     + geom_histogram(aes(x = p))
     + labs(x = "p-value", y = "Frequency")
     + theme_minimal()
 )
 ggsave(
-    "Plots/sv-disruption.acetylation.p-values.png",
+    "Plots/sv-disruption/acetylation.p-values.png",
+    gg_pval_hist,
+    height = 12,
+    width = 20,
+    units = "cm"
+)
+ggsave(
+    "Plots/sv-disruption/acetylation.p-values.pdf",
+    gg_pval_hist,
     height = 12,
     width = 20,
     units = "cm"
 )
 
 
-gg = (
+gg_volcano = (
     ggplot(data = acetyl)
     + geom_point(aes(x = z, y = -log10(padj)))
     + geom_hline(aes(yintercept = -log10(0.05)), linetype = "dashed")
@@ -109,8 +160,47 @@ gg = (
     + theme_minimal()
 )
 ggsave(
-    "Plots/sv-disruption.acetylation.volcano.png",
+    "Plots/sv-disruption/acetylation.volcano.png",
+    gg_volcano,
     height = 12,
     width = 20,
     units = "cm"
 )
+ggsave(
+    "Plots/sv-disruption/acetylation.volcano.pdf",
+    gg_volcano,
+    height = 12,
+    width = 20,
+    units = "cm"
+)
+
+gg_all_tests_pval_hist <- (
+    ggplot(data = test_group_acetyl)
+    + geom_density(aes(x = p, colour = test_ID, group = test_ID))
+    + labs(x = "p-value", y = "Frequency")
+    + theme_minimal()
+)
+ggsave(
+    "Plots/sv-disruption/acetylation.p-values.all-tests.png",
+    gg_all_tests_pval_hist,
+    height = 12,
+    width = 20,
+    units = "cm"
+)
+
+for (tid in tests[, test_ID]) {
+    print(tid)
+    gg_test_pval_hist <- (
+        ggplot(data = test_group_acetyl[test_ID == tid, .SD])
+        + geom_density(aes(x = p))
+        + labs(x = "p-value", y = "Density")
+        + theme_minimal()
+    )
+    ggsave(
+        paste0("Acetylation/Tests/p-values.", tid, ".png"),
+        gg_test_pval_hist,
+        height = 12,
+        width = 20,
+        units = "cm"
+    )
+}
