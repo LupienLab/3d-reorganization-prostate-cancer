3cf010368a456c5c3eaca2930399f3de085d295f
James Hawley <jamesrichardhawley@gmail.com>
2020-06-16 15:41:08 -0400

    Updated plots from acetylation results

--- Results/2020-06-12_sv-disruption-acetylation/plot-acetylation.R
+++ Results/2020-06-12_sv-disruption-acetylation/plot-acetylation.R
@@ -5,6 +5,9 @@ suppressMessages(library("data.table"))
 suppressMessages(library("pheatmap"))
 suppressMessages(library("RColorBrewer"))
 suppressMessages(library("ggplot2"))
+source("../2020-02-19_chromoplexy/plotting-helper.R")
+
+PLOT_DIR <- "Plots"
 
 # ==============================================================================
 # Data
@@ -21,57 +24,21 @@ metadata[, ChIP_file := paste0("../../Data/Processed/2019-05-03_PCa-H3K27ac-peak
 metadata[, Ctrl_file := paste0("../../Data/Processed/2019-05-03_PCa-H3K27ac-peaks/BAMs/Pca", get("Sample ID"), "_input.sorted.dedup.bam")]
 SAMPLES <- metadata[, SampleID]
 
-# load raw counts of H3K27ac pull down and input, then take differences and get total ChIP library sizes
-library_sizes <- sapply(SAMPLES, function(s) 0)
-for (s in SAMPLES) {
-    dt_chip <- fread(
-        file.path("Acetylation", paste0(s, "_H3K27ac.induced-region-counts.bed")),
-        sep = "\t",
-        header = FALSE,
-        col.names = c("chr", "start", "end", "count", "supported", "width", "frac_supported")
-    )
-    dt_input <- fread(
-        file.path("Acetylation", paste0(s, "_input.induced-region-counts.bed")),
-        sep = "\t",
-        header = FALSE,
-        col.names = c("chr", "start", "end", "count", "supported", "width", "frac_supported")
-    )
-    # calculate the linear scaling based on library size
-}
-library_sizes <- sapply(SAMPLES, function(s) 0)
-for (s in SAMPLES) {
-    dt_chip <- fread(
-        file.path("Acetylation", paste0(s, "_H3K27ac.induced-region-counts.bed")),
-        sep = "\t",
-        header = FALSE,
-        col.names = c("chr", "start", "end", "count", "supported", "width", "frac_supported")
-    )
-    dt_input <- fread(
-        file.path("Acetylation", paste0(s, "_input.induced-region-counts.bed")),
-        sep = "\t",
-        header = FALSE,
-        col.names = c("chr", "start", "end", "count", "supported", "width", "frac_supported")
-    )
-    # calculate the linear scaling based on library size
-    library_sizes[s] <- min(dt_chip[, sum(count)], dt_input[, sum(count)])
-}
- 
-
 # load TAD acetylation hypothesis test values
 acetyl <- fread(
-    "Graphs/sv-disruption-tests.acetylation.tsv",
+    "sv-disruption-tests.acetylation.tsv",
     sep = "\t",
     header = TRUE
 )
 
-tests <- fread(
-    "Graphs/sv-disruption-tests.tsv",
+size_factors <- fread(
+    "sv-disruption-tests.acetylation.size-factors.tsv",
     sep = "\t",
     header = TRUE
 )
 
 test_group_acetyl <- rbindlist(lapply(
-    tests[, test_ID],
+    acetyl[, test_ID],
     function(tid) {
         dt <- fread(
             paste0("Acetylation/Tests/test_", tid, ".results.tsv"),
@@ -82,6 +49,8 @@ test_group_acetyl <- rbindlist(lapply(
         return(dt)
     }
 ))
+
+
 # ==============================================================================
 # Plots
 # ==============================================================================
@@ -100,77 +69,44 @@ pheatmap(
     clustering_method = "ward.D2",
     annotation_col = ann_cols,
     legend = TRUE,
-    filename = "Plots/H3K27ac-correlation-tad-induced.png"
+    filename = file.path(PLOT_DIR, "H3K27ac-correlation-tad-induced.png")
 )
 
-gg_qq = (
+gg_qq <- (
     ggplot(data = acetyl[order(p)])
     + geom_point(aes(x = -log10(ppoints(acetyl[, .N])), y = -log10(p)))
     + geom_abline(aes(intercept = 0, slope = 1), linetype = "dashed")
+    + scale_x_continuous(
+        limits = c(0, 3)
+    )
+    + scale_y_continuous(
+        limits = c(0, 15)
+    )
     + labs(
         x = expression(-log[10] * "(Uniform quantile)"),
         y = expression(-log[10] * "(Observed quantile)")
     )
     + theme_minimal()
 )
-ggsave(
-    "Plots/sv-disruption/acetylation.qq-plot.png",
-    gg_qq,
-    height = 12,
-    width = 20,
-    units = "cm"
-)
-ggsave(
-    "Plots/sv-disruption/acetylation.qq-plot.pdf",
-    gg_qq,
-    height = 12,
-    width = 20,
-    units = "cm"
-)
+savefig(gg_qq, file.path(PLOT_DIR, "acetylation.qq-plot"))
 
-gg_pval_hist = (
+gg_pval_hist <- (
     ggplot(data = acetyl)
     + geom_histogram(aes(x = p))
     + labs(x = "p-value", y = "Frequency")
     + theme_minimal()
 )
-ggsave(
-    "Plots/sv-disruption/acetylation.p-values.png",
-    gg_pval_hist,
-    height = 12,
-    width = 20,
-    units = "cm"
-)
-ggsave(
-    "Plots/sv-disruption/acetylation.p-values.pdf",
-    gg_pval_hist,
-    height = 12,
-    width = 20,
-    units = "cm"
-)
+savefig(gg_pval_hist, file.path(PLOT_DIR, "acetylation.p-values"))
 
 
-gg_volcano = (
+gg_volcano <- (
     ggplot(data = acetyl)
     + geom_point(aes(x = z, y = -log10(padj)))
     + geom_hline(aes(yintercept = -log10(0.05)), linetype = "dashed")
     + labs(x = "Stouffer's Z", y = expression(-log[10] * " FDR"))
     + theme_minimal()
 )
-ggsave(
-    "Plots/sv-disruption/acetylation.volcano.png",
-    gg_volcano,
-    height = 12,
-    width = 20,
-    units = "cm"
-)
-ggsave(
-    "Plots/sv-disruption/acetylation.volcano.pdf",
-    gg_volcano,
-    height = 12,
-    width = 20,
-    units = "cm"
-)
+savefig(gg_volcano, file.path(PLOT_DIR, "acetylation.volcano"))
 
 gg_all_tests_pval_hist <- (
     ggplot(data = test_group_acetyl)
@@ -178,15 +114,9 @@ gg_all_tests_pval_hist <- (
     + labs(x = "p-value", y = "Frequency")
     + theme_minimal()
 )
-ggsave(
-    "Plots/sv-disruption/acetylation.p-values.all-tests.png",
-    gg_all_tests_pval_hist,
-    height = 12,
-    width = 20,
-    units = "cm"
-)
+savefig(gg_all_tests_pval_hist, file.path(PLOT_DIR, "acetylation.p-values.all-tests"))
 
-for (tid in tests[, test_ID]) {
+for (tid in acetyl[, test_ID]) {
     print(tid)
     gg_test_pval_hist <- (
         ggplot(data = test_group_acetyl[test_ID == tid, .SD])
@@ -194,11 +124,5 @@ for (tid in tests[, test_ID]) {
         + labs(x = "p-value", y = "Density")
         + theme_minimal()
     )
-    ggsave(
-        paste0("Acetylation/Tests/p-values.", tid, ".png"),
-        gg_test_pval_hist,
-        height = 12,
-        width = 20,
-        units = "cm"
-    )
+    savefig(gg_test_pval_hist, file.path(PLOT_DIR, "Tests", paste0("acetylation.p-values.test_", tid)))
 }
