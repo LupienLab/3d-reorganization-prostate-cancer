320f5eb4874bd8b001ea6d07c0c9e2d70d4dc9c6
James Hawley <jamesrichardhawley@gmail.com>
2020-08-14 17:25:38 -0400

    Updating breakpoint figures, output summary tables
    * Refactoring summary analyses from plotting
    * Slight change to plot names
    * Code + new exported tables

--- Results/2020-02-19_chromoplexy/plot-breaks.R
+++ Results/2020-02-19_chromoplexy/plot-breaks.R
@@ -9,36 +9,17 @@ source("plotting-helper.R")
 
 CHRS = paste0('chr', c(1:22, "X", "Y"))
 
-hg38 = data.table(
-    Chrom = factor(CHRS, levels = CHRS, ordered = TRUE),
-    Length = c(
-        248956422,
-        242193529,
-        198295559,
-        190214555,
-        181538259,
-        170805979,
-        159345973,
-        145138636,
-        138394717,
-        133797422,
-        135086622,
-        133275309,
-        114364328,
-        107043718,
-        101991189,
-        90338345,
-        83257441,
-        80373285,
-        58617616,
-        64444167,
-        46709983,
-        50818468,
-        156040895,
-        57227415
-    )
+hg38 = fread(
+    file.path("..", "..", "Data", "Processed", "2019-06-18_PCa-LowC-sequencing", "hg38.sizes.txt"),
+    sep = "\t",
+    header = FALSE,
+    col.names = c("Chrom", "Length")
 )
 hg38[, Bins := ceiling(Length / 10^6)]
+
+STATS_DIR <- "Statistics"
+PLOT_DIR <- "Plots"
+
 
 # ==============================================================================
 # Data
@@ -58,139 +39,39 @@ breakpoint_pairs <- fread("Graphs/sv-breakpoints.paired.tsv", sep = "\t", header
 # load hg38 ideogram
 data(UCSC.HG38.Human.CytoBandIdeogram)
 
+# load summary data
+breakpoints_by_chrom <- fread(
+    file.path(STATS_DIR, "breakpoints.by-chrom.tsv"),
+    sep = "\t",
+    header = TRUE,
+    col.names = c("SampleID", "T2E_Status", "chr", "N", "N_per_mb")
+)
+
+breakpoints_summed <- fread(
+    file.path(STATS_DIR, "breakpoints.binned.tsv")
+)
+
+inter_intra_counts <- fread(
+    file.path(STATS_DIR, "breakpoint-pairs.inter-intra-chromosomal.tsv")
+)
+
+sv_components <- fread(
+    file.path(STATS_DIR, "sv-components.tsv")
+)
+
+sv_components_counted <- fread(
+    file.path(STATS_DIR, "sv-components.counts.tsv")
+)
+
+htests <- readRDS(file.path(STATS_DIR, "htests.rds"))
+
+
 # ==============================================================================
 # Analysis
 # ==============================================================================
-# convert breakpoints into Mbp bins and counts the rearrangements
-breakpoints[, StartBin := floor(start / 10^6)]
-breakpoints[, EndBin := floor(end / 10^6)]
-
-# add T2E status
-breakpoints <- merge(
-    breakpoints,
-    metadata[, .SD, .SDcols = c("SampleID", "T2E Status")],
-    by = "SampleID"
-)
-
-# count breakpoints per sample
-breakpoints_counted <- breakpoints[, .N, keyby = c("SampleID", "T2E Status")]
-
-# convert breakpoints in genomic regions into bins
-breakpoints_binned = rbindlist(lapply(
-    1:breakpoints[, .N],
-    function(i) {
-        start_bin = breakpoints[i, StartBin]
-        end_bin = breakpoints[i, EndBin]
-        dt = data.table(
-            SampleID = breakpoints[i, SampleID],
-            chr = breakpoints[i, chr],
-            Bin = start_bin:end_bin,
-            Count = 1
-        )
-        return(dt)
-    }
-))
-# count all breakpoints in each bin
-breakpoints_summed = breakpoints_binned[, sum(Count), by = c("SampleID", "chr", "Bin")]
-colnames(breakpoints_summed) = c("SampleID", "chr", "Bin", "Count")
-fwrite(
-    breakpoints_summed[order(SampleID, chr, Bin)],
-    "Statistics/breakpoints.binned.tsv",
-    sep = "\t",
-    col.names = TRUE
-)
-
-breakpoints_by_chrom = breakpoints[, .N, keyby = c("SampleID", "chr")]
-breakpoints_by_chrom[, N_per_mb := apply(.SD, 1, function(r) {as.numeric(r["N"]) / hg38[Chrom == r["chr"], (Length / 10^6)]})]
-
-# merge T2E information
-breakpoints_by_chrom <- merge(
-    x = breakpoints_by_chrom,
-    y = metadata[, .(SampleID, T2E_Status = get("T2E Status"))],
-    by = "SampleID"
-)
-fwrite(
-    breakpoints_by_chrom[order(SampleID, chr)],
-    "Statistics/breakpoints.by-chrom.tsv",
-    sep = "\t",
-    col.names = TRUE
-)
-
-
-# count the number of inter-/intra-chromosomal pairs
-inter_intra_counts <- breakpoint_pairs[, .(Total = .N), by = "SampleID"]
-inter_intra_counts$Intrachromosomal <- breakpoint_pairs[, chr_x == chr_y, by = "SampleID"][, sum(V1), by = "SampleID"]$V1
-inter_intra_counts$Interchromosomal <- breakpoint_pairs[, chr_x != chr_y, by = "SampleID"][, sum(V1), by = "SampleID"]$V1
-inter_intra_counts$T2E_Status <- metadata[, get("T2E Status")] # these are in the same order, so I don't need to merge
-inter_intra_counts <- melt(
-    inter_intra_counts,
-    id.vars = c("SampleID", "T2E_Status"),
-    variable.name = "Class",
-    value.name = "Count"
-)
-
-fwrite(
-    inter_intra_counts,
-    "Statistics/breakpoint-pairs.inter-intra-chromosomal.tsv",
-    sep = "\t",
-    col.names = TRUE
-)
-
-# calculate the length of events in each sample (i.e. size of each connected component)
-breakpoint_components <- breakpoints[, .N, by = c("SampleID", "component_ID")]
-
-# merge metadata for plotting
-breakpoint_components <- merge(
-    x = breakpoint_components,
-    y = metadata[, .SD, .SDcols = c("SampleID", "T2E Status")],
-    by = "SampleID"
-)
-
-breakpoint_components_counted <- breakpoint_components[, .N, by = c("SampleID", "T2E Status")]
-colnames(breakpoint_components_counted)[3] <- "Events"
-breakpoint_components_counted$Complex_Events <- breakpoint_components[,
-    N > 2,
-    by = "SampleID"
-    ][, sum(V1), by = "SampleID"]$V1
-
-# perform Mann-Whitney U test to see if the T2E samples have more complex events than the non-T2E samples
-htest_breaks <- wilcox.test(
-    x = breakpoints_counted[get("T2E Status") == "No", N],
-    y = breakpoints_counted[get("T2E Status") == "Yes", N],
-    alternative = "less"
-)
-
-htest_events <- list(
-    "total" = wilcox.test(
-        x = breakpoint_components_counted[get("T2E Status") == "No", Events],
-        y = breakpoint_components_counted[get("T2E Status") == "Yes", Events],
-        alternative = "less"
-    ),
-    "complex" = wilcox.test(
-        x = breakpoint_components_counted[get("T2E Status") == "No", Complex_Events],
-        y = breakpoint_components_counted[get("T2E Status") == "Yes", Complex_Events],
-        alternative = "less"
-    )
-)
-
-# calculate the number of chromosomes involved in an SV
-sv_chrom_span <- breakpoints[, length(unique(chr)), by = c("SampleID", "component_ID")]
-sv_chrom_span <- merge(
-    sv_chrom_span,
-    breakpoint_components,
-    by = c("SampleID", "component_ID")
-)
-
-
-fisher.test(
-    rbind(
-        c(inter_intra_counts[Class == "Intrachromosomal" & T2E_Status == "No", sum(Count)], inter_intra_counts[Class == "Intrachromosomal" & T2E_Status == "Yes", sum(Count)]),
-        c(inter_intra_counts[Class == "Interchromosomal" & T2E_Status == "No", sum(Count)], inter_intra_counts[Class == "Interchromosomal" & T2E_Status == "Yes", sum(Count)])
-    ),
-    alternative = "two.sided"
-)
-
+# calculate the distance between intra-chromosomal breakpoint pairs
 breakpoint_pairs[chr_x == chr_y, dist := pmin(abs(start_y - end_x), abs(start_x - end_y))]
+
 
 # ==============================================================================
 # Plots
@@ -277,7 +158,7 @@ gg_breakpoints <- (
         axis.text.x = element_text(angle = 90, vjust = 0, hjust = 0.5)
     )
 )
-savefig(gg_breakpoints, "Plots/breakpoint-stats/sv-counts")
+savefig(gg_breakpoints, "Plots/breakpoint-stats/breakpoint-counts")
 
 gg_breakpoints_per_chrom = (
     ggplot(data = breakpoints_by_chrom)
@@ -305,7 +186,7 @@ gg_breakpoints_per_chrom = (
         legend.position = "bottom"
     )
 )
-savefig(gg_breakpoints_per_chrom, "Plots/breakpoint-stats/sv-counts.by-chrom.labelled")
+savefig(gg_breakpoints_per_chrom, file.path(PLOT_DIR, "breakpoint-stats", "breakpoint-counts.by-chrom.labelled"))
 
 # save as above without any text
 gg_breakpoints_per_chrom_unlabelled = (
@@ -317,36 +198,9 @@ gg_breakpoints_per_chrom_unlabelled = (
         strip.text.x = element_blank()
     )
 )
-savefig(gg_breakpoints_per_chrom_unlabelled, "Plots/breakpoint-stats/sv-counts.by-chrom")
+savefig(gg_breakpoints_per_chrom_unlabelled, file.path(PLOT_DIR, "breakpoint-stats", "breakpoint-counts.by-chrom"))
 
-# number and location of SVs across all patients
-gg_breakpoints_summed = (
-    ggplot()
-    + geom_col(
-        data = breakpoints_summed,
-        mapping = aes(x = Bin, y = Count, fill = SampleID),
-        position = "stack"
-    )
-    # add geom_blank to ensure each facet is the correct size
-    + geom_blank(data = hg38, mapping = aes(x = Bins, y = 0))
-    + labs(x = "Position", y = "Count")
-    + scale_fill_manual(
-        breaks = metadata[, SampleID],
-        labels = metadata[, get("Patient ID")],
-        values = metadata[, Colour],
-        name = "Patient"
-    )
-    + facet_grid(. ~ chr, scales = "free_x", space = "free_x", switch = "x")
-    + theme_classic()
-    + theme(
-        axis.text.x = element_blank(),
-        axis.ticks.x = element_blank(),
-        panel.spacing = unit(0, "lines"),
-        strip.text.x = element_text(angle = 90, vjust = 0.5, hjust = 0.5)
-    )
-)
-savefig(gg_breakpoints_summed, "Plots/breakpoint-stats/sv-loci", width = 40)
-
+# distance distribution between intra-chromosomal breakpoint pairs
 gg_breakpoint_pair_dist <- (
     ggplot(data = breakpoint_pairs)
     + geom_histogram(aes(x = dist / 1e6, binwidth = 10))
@@ -354,7 +208,7 @@ gg_breakpoint_pair_dist <- (
     + labs(x = "Breakpoint pair distance (Mbp)", y = "Frequency")
     + theme_minimal()
 )
-savefig(gg_breakpoint_pair_dist, "Plots/breakpoint-stats/sv-pair-distance-distribution")
+savefig(gg_breakpoint_pair_dist, file.path(PLOT_DIR, "breakpoint-stats", "breakpoint-pairs.distance-distribution"))
 
 
 # megabase bins containing multiple breakpoints across samples
@@ -369,8 +223,9 @@ gg_recurrence = (
     )
     + theme_minimal()
 )
-savefig(gg_recurrence, "Plots/breakpoint-stats/sv-recurrence")
+savefig(gg_recurrence, "Plots/breakpoint-stats/breakpoint-recurrence")
 
+# total number of inter-/intra-chromosomal breakpoint pairs
 gg_inter_intra = (
     ggplot(data = inter_intra_counts[Class != "Total"])
     + geom_col(
@@ -461,9 +316,9 @@ savefig(gg_inter_intra_t2e_comp, "Plots/breakpoint-stats/breakpoint-pairs.inter-
 # 3. length and distribution of complex events
 # --------------------------------------
 gg_component_length = (
-    ggplot(data = breakpoint_components)
+    ggplot(data = sv_components)
     + geom_bar(
-        aes(x = N, group = SampleID, fill = SampleID),
+        aes(x = N_Breakpoints, group = SampleID, fill = SampleID),
         position = position_dodge(preserve = "single")
     )
     + scale_fill_manual(
@@ -473,21 +328,21 @@ gg_component_length = (
         name = "Patient"
     )
     + labs(x = "Breakpoints in SV", y = "Count")
-    + guides(group = FALSE, fill = guide_legend(title = "Patients"))
+    + guides(group = FALSE, fill = guide_legend(title = "Patient"))
     + theme_minimal()
 )
 savefig(gg_component_length, "Plots/breakpoint-stats/sv-events.size")
 
 # number of SV events detected in total
-gg_t2e_components = (
-    ggplot(data = breakpoint_components_counted)
+gg_t2e_components <- (
+    ggplot(data = sv_components_counted)
     + geom_boxplot(
-        aes(x = get("T2E Status"), y = Events, colour = get("T2E Status")),
+        aes(x = get("T2E Status"), y = N_Events, colour = get("T2E Status")),
         width = 0.5,
         outlier.shape = NA
     )
     + geom_point(
-        aes(x = get("T2E Status"), y = Events, colour = get("T2E Status")),
+        aes(x = get("T2E Status"), y = N_Events, colour = get("T2E Status")),
         position = position_jitter(width = 0.1, height = 0)
     )
     + geom_path(
@@ -499,7 +354,7 @@ gg_t2e_components = (
     )
     + annotate(
         geom = "text",
-        label = paste("p =", scientific(htest_events[["total"]]$p.value, digits = 3)),
+        label = paste("p =", scientific(htests[["total"]]$p.value, digits = 3)),
         x = 1.5,
         y = 38,
         vjust = -1
@@ -520,55 +375,12 @@ gg_t2e_components = (
 )
 savefig(gg_t2e_components, "Plots/breakpoint-stats/sv-events.total.T2E-comparison", height = 20, width = 12)
 
-gg_t2e_components_bps = (
-    ggplot(data = breakpoints[, .N, by = c("SampleID", "T2E Status")])
-    + geom_boxplot(
-        aes(x = get("T2E Status"), y = N, colour = get("T2E Status")),
-        width = 0.5,
-        outlier.shape = NA
-    )
-    + geom_point(
-        aes(x = get("T2E Status"), y = N, colour = get("T2E Status")),
-        position = position_jitter(width = 0.1, height = 0)
-    )
-    + geom_path(
-        data = data.table(
-            x = rep(c("No", "Yes"), each = 2),
-            y = c(25, 98, 98, 96)
-        ),
-        aes(x = x, y = y, group = 1)
-    )
-    + annotate(
-        geom = "text",
-        label = paste("p =", scientific(htest_breaks$p.value, digits = 3)),
-        x = 1.5,
-        y = 98,
-        vjust = -1
-    )
-    + labs(x = NULL, y = "Unique Breakpoints")
-    + scale_x_discrete(
-        breaks = c("No", "Yes"),
-        labels = c("T2E-", "T2E+")
-    )
-    + scale_y_continuous(
-        limits = c(0, 100)
-    )
-    + scale_colour_manual(
-        breaks = c("No", "Yes"),
-        labels = c("T2E-", "T2E+"),
-        values = c("#2A363B", "#019875")
-    )
-    + guides(colour = FALSE)
-    + theme_minimal()
-)
-savefig(gg_t2e_components_bps, "Plots/breakpoint-stats/sv-breakpoints.T2E-comparison", height = 20, width = 12)
-
 # number of complex components
 gg_t2e_components_complex = (
-    ggplot(data = breakpoint_components_counted)
-    + geom_boxplot(aes(x = get("T2E Status"), y = Complex_Events, colour = get("T2E Status")), width = 0.5)
+    ggplot(data = sv_components_counted)
+    + geom_boxplot(aes(x = get("T2E Status"), y = N_Complex_Events, colour = get("T2E Status")), width = 0.5)
     + geom_point(
-        aes(x = get("T2E Status"), y = Complex_Events, colour = get("T2E Status")),
+        aes(x = get("T2E Status"), y = N_Complex_Events, colour = get("T2E Status")),
         position = position_jitter(width = 0.1, height = 0)
     )
     + geom_path(
@@ -580,7 +392,7 @@ gg_t2e_components_complex = (
     )
     + annotate(
         geom = "text",
-        label = paste("p =", scientific(htest_events[["complex"]]$p.value, digits = 3)),
+        label = paste("p =", scientific(htests[["complex"]]$p.value, digits = 3)),
         x = 1.5,
         y = 13,
         vjust = -1
@@ -603,8 +415,8 @@ savefig(gg_t2e_components_complex, "Plots/breakpoint-stats/sv-events.complex.T2E
 
 # number of chromosomes that events span
 gg_sv_chrom_span <- (
-    ggplot(data = sv_chrom_span[N > 2, .(V2 = .N), keyby = "V1"])
-    + geom_col(aes(x = V1, y = V2, fill = V1))
+    ggplot(data = sv_components[, .N, by = N_Chr])
+    + geom_col(aes(x = N_Chr, y = N, fill = N_Chr))
     + labs(x = "Chromosomes in a complex SV", y = "Count")
     + guides(fill = FALSE)
     + scale_fill_viridis_c()
---
+++ Results/2020-02-19_chromoplexy/summarize-breaks.R
@@ -0,0 +1,235 @@
+# ==============================================================================
+# Meta
+# ==============================================================================
+# Summary breaks
+# --------------------------------------
+# Description: Summarizes the breakpoints and SV events in each patient
+# Author: James Hawley
+
+
+# ==============================================================================
+# Environment
+# ==============================================================================
+suppressMessages(library("data.table"))
+suppressMessages(library("ggplot2"))
+
+CHRS = paste0('chr', c(1:22, "X", "Y"))
+
+hg38 = fread(
+    file.path("..", "..", "Data", "Processed", "2019-06-18_PCa-LowC-sequencing", "hg38.sizes.txt"),
+    sep = "\t",
+    header = FALSE,
+    col.names = c("Chrom", "Length")
+)
+hg38[, Bins := ceiling(Length / 10^6)]
+
+
+# ==============================================================================
+# Data
+# ==============================================================================
+# load metadata
+metadata <- fread(
+    file.path("..", "..", "Data", "External", "LowC_Samples_Data_Available.tsv"),
+    sep = "\t",
+    header = TRUE
+)
+metadata <- metadata[Include == "Yes"]
+metadata[, SampleID := paste0("PCa", get("Sample ID"))]
+SAMPLES <- metadata$SampleID
+
+# load breakpoint data
+breakpoints <- fread(file.path("Graphs", "sv-breakpoints.tsv"), sep = "\t", header = TRUE)
+breakpoint_pairs <- fread(file.path("Graphs", "sv-breakpoints.paired.tsv"), sep = "\t", header = TRUE)
+
+
+# ==============================================================================
+# Analysis
+# ==============================================================================
+# 1. Initial data cleaning
+# --------------------------------------
+# convert breakpoints into Mbp bins and counts the rearrangements
+breakpoints[, StartBin := floor(start / 10^6)]
+breakpoints[, EndBin := floor(end / 10^6)]
+
+# add T2E status
+breakpoints <- merge(
+    breakpoints,
+    metadata[, .SD, .SDcols = c("SampleID", "T2E Status")],
+    by = "SampleID"
+)
+
+breakpoint_pairs <- merge(
+    breakpoint_pairs,
+    metadata[, .SD, .SDcols = c("SampleID", "T2E Status")],
+    by = "SampleID"
+)
+
+
+# 2. Counting breakpoints
+# --------------------------------------
+cat("Summarizing breakpoints\n")
+# count breakpoints per sample
+breakpoints_counted <- breakpoints[, .N, keyby = c("SampleID", "T2E Status")]
+
+# convert breakpoints in genomic regions into bins
+breakpoints_binned = rbindlist(lapply(
+    1:breakpoints[, .N],
+    function(i) {
+        start_bin = breakpoints[i, StartBin]
+        end_bin = breakpoints[i, EndBin]
+        dt = data.table(
+            SampleID = breakpoints[i, SampleID],
+            chr = breakpoints[i, chr],
+            Bin = start_bin:end_bin,
+            Count = 1
+        )
+        return(dt)
+    }
+))
+# count all breakpoints in each bin
+breakpoints_summed = breakpoints_binned[, sum(Count), by = c("SampleID", "chr", "Bin")]
+colnames(breakpoints_summed) = c("SampleID", "chr", "Bin", "Count")
+fwrite(
+    breakpoints_summed[order(SampleID, chr, Bin)],
+    file.path("Statistics", "breakpoints.binned.tsv"),
+    sep = "\t",
+    col.names = TRUE
+)
+
+# count breakpoints per chromosome
+breakpoints_by_chrom = breakpoints[, .N, keyby = c("SampleID", "T2E Status", "chr")]
+breakpoints_by_chrom[, N_per_mb := apply(.SD, 1, function(r) {as.numeric(r["N"]) / hg38[Chrom == r["chr"], (Length / 10^6)]})]
+fwrite(
+    breakpoints_by_chrom[order(SampleID, chr)],
+    file.path("Statistics", "breakpoints.by-chrom.tsv"),
+    sep = "\t",
+    col.names = TRUE
+)
+
+
+# 2. Counting breakpoint pairs
+# --------------------------------------
+cat("Summarizing breakpoint pairs\n")
+# count the number of inter-/intra-chromosomal pairs
+inter_intra_counts <- unique(breakpoint_pairs[,
+    .(
+        T2E_Status = get("T2E Status"),
+        Total = .N,
+        Intrachromosomal = sum(chr_x == chr_y),
+        Interchromosomal = sum(chr_x != chr_y)
+    ),
+    by = "SampleID"
+])
+inter_intra_counts <- melt(
+    inter_intra_counts,
+    id.vars = c("SampleID", "T2E_Status"),
+    variable.name = "Class",
+    value.name = "Count"
+)
+fwrite(
+    inter_intra_counts,
+    file.path("Statistics", "breakpoint-pairs.inter-intra-chromosomal.tsv"),
+    sep = "\t",
+    col.names = TRUE
+)
+
+
+# 3. Counting SV events (simple and complex)
+# --------------------------------------
+cat("Summarizing SV events\n")
+# calculate the length of events in each sample (i.e. size of each connected component)
+breakpoint_components <- breakpoints[,
+    .(
+        N_Breakpoints = .N,
+        N_Chr = length(unique(chr))
+    ),
+    by = c("SampleID", "T2E Status", "component_ID")
+]
+fwrite(
+    breakpoint_components,
+    file.path("Statistics", "sv-components.tsv"),
+    sep = "\t",
+    col.names = TRUE
+)
+
+# calculate the number of total events and complex events
+breakpoint_components_counted <- breakpoint_components[,
+    .(
+        N_Events = .N,
+        N_Complex_Events = sum(N_Breakpoints > 2)
+    ),
+    by = c("SampleID", "T2E Status")
+]
+fwrite(
+    breakpoint_components_counted,
+    file.path("Statistics", "sv-components.counts.tsv"),
+    sep = "\t",
+    col.names = TRUE
+)
+
+# 4. Hypothesis testing
+# --------------------------------------
+cat("Hypothesis tests\n")
+# perform Mann-Whitney U to test if T2E patients have more of the following than non-T2E:
+#   a) unique breakpoints
+#   b) total events
+#   c) complex events
+#   d) Inter-chromosomal breakpoint pairs
+#   e) Intra-chromosomal breakpoint pairs
+#   f) independence between inter-/intra-chromosomal pairs and T2E status
+
+htests <- list(
+    # a)
+    "breakpoints" = wilcox.test(
+        x = breakpoints_counted[get("T2E Status") == "No", N],
+        y = breakpoints_counted[get("T2E Status") == "Yes", N],
+        alternative = "less"
+    ),
+    # b) 
+    "total" = wilcox.test(
+        x = breakpoint_components_counted[get("T2E Status") == "No", N_Events],
+        y = breakpoint_components_counted[get("T2E Status") == "Yes", N_Events],
+        alternative = "less"
+    ),
+    # c)
+    "complex" = wilcox.test(
+        x = breakpoint_components_counted[get("T2E Status") == "No", N_Complex_Events],
+        y = breakpoint_components_counted[get("T2E Status") == "Yes", N_Complex_Events],
+        alternative = "less"
+    ),
+    # d)
+    "inter" = wilcox.test(
+        x = inter_intra_counts[T2E_Status == "No" & Class == "Interchromosomal", Count],
+        y = inter_intra_counts[T2E_Status == "Yes" & Class == "Interchromosomal", Count],
+        alternative = "less"
+    ),
+    # e)
+    "intra" = wilcox.test(
+        x = inter_intra_counts[T2E_Status == "No" & Class == "Intrachromosomal", Count],
+        y = inter_intra_counts[T2E_Status == "Yes" & Class == "Intrachromosomal", Count],
+        alternative = "less"
+    ),
+    # f)
+    "t2e_chr" = fisher.test(
+        rbind(
+            c(inter_intra_counts[Class == "Intrachromosomal" & T2E_Status == "No", sum(Count)], inter_intra_counts[Class == "Intrachromosomal" & T2E_Status == "Yes", sum(Count)]),
+            c(inter_intra_counts[Class == "Interchromosomal" & T2E_Status == "No", sum(Count)], inter_intra_counts[Class == "Interchromosomal" & T2E_Status == "Yes", sum(Count)])
+        ),
+        alternative = "two.sided"
+    )
+)
+# save hypothesis test results
+saveRDS(htests, file.path("Statistics", "htests.rds"))
+
+cat("----\n\tTotal breakpoints:\n")
+print(htests$breakpoints)
+cat("----\n\tTotal events:\n")
+print(htests$total)
+cat("----\n\tComplex events:\n")
+print(htests$complex)
+cat("----\n\tInter-chromosomal pairs:\n")
+print(htests$inter)
+cat("----\n\tIntra-chromosomal pairs:\n")
+print(htests$intra)
+cat("----\n\tBreakpoint pair location and T2E status:\n")
+print(htests$t2e_chr)
