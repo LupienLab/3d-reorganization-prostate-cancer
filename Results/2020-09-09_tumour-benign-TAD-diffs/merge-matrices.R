2ca836198d86396f15d8ba2577c1200a9c56f344
James Hawley <jamesrichardhawley@gmail.com>
2020-09-09 15:09:35 -0400

    Minor update to merge-matrices

--- Results/2020-09-09_tumour-benign-TAD-diffs/merge-matrices.R
+++ Results/2020-09-09_tumour-benign-TAD-diffs/merge-matrices.R
@@ -8,28 +8,6 @@ suppressMessages(library("Matrix"))
 
 CHRS <- paste0("chr", c(1:22, "X"))
 
-# ==============================================================================
-# Functions
-# ==============================================================================
-#' Save figures in multiple formats
-#'
-#' @param gg ggplot object
-#' @param prefix Prefix for output file
-#' @param ext Output extensions
-#' @param dpi DPI resolution
-savefig = function(gg, prefix, ext = c("png", "pdf"), width = 20, height = 12, dpi = 400) {
-    for (e in ext) {
-        ggsave(
-            paste(prefix, e, sep = "."),
-            gg,
-            height = height,
-            width = width,
-            units = "cm",
-            dpi = dpi
-        )
-    }
-}
-
 
 # ==============================================================================
 # Data
@@ -40,6 +18,7 @@ metadata <- fread("config.tsv", sep = "\t", header = TRUE)
 metadata <- metadata[Include == "Yes"]
 TUMOUR_SAMPLES <- metadata[Type == "Malignant", SampleID]
 BENIGN_SAMPLES <- metadata[Type == "Benign", SampleID]
+
 
 # ==============================================================================
 # Analysis
