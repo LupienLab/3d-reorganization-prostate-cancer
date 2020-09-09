305489df3058a491ae780b10cd12e96d04b3b327
James Hawley <jamesrichardhawley@gmail.com>
2020-09-09 17:57:36 -0400

    Code for merging matrices from different samples

---
+++ Results/2020-09-09_tumour-benign-TAD-diffs/README.md
@@ -0,0 +1,5 @@
+# Summary
+
+Based on the results from [`../2020-08-29_TADs-downsampled/`](../2020-08-29_TADs-downsampled/), we suspect that we can identify differences in TAD differences between benign and tumour samples based on their location.
+To do this, we use a differential interaction/TAD caller, TADsplimer, to identify where this might occur.
+
--- Results/2020-09-09_tumour-benign-TAD-diffs/merge-matrices.R
+++ Results/2020-09-09_tumour-benign-TAD-diffs/merge-matrices.R
@@ -2,11 +2,32 @@
 # Environment
 # ==============================================================================
 suppressMessages(library("data.table"))
-suppressMessages(library("scales"))
-suppressMessages(library("ggplot2"))
-suppressMessages(library("Matrix"))
+suppressMessages(library("MASS"))
+
 
 CHRS <- paste0("chr", c(1:22, "X"))
+
+
+# ==============================================================================
+# Functions
+# ==============================================================================
+# simplify the process of reading in exported matrix files
+read_contacts <- function(id, chrom) {
+    # read zipped matrix file
+    dt <- fread(
+        cmd = paste0("zcat TMP/", id, ".300000000.res_40000bp.", chrom, ".noheader.mtx.gz"),
+        sep = "\t"
+    )
+    # for some reason the last column is filled with NAs, but these shouldn't be here
+    na_cols <- dt[, which(apply(.SD, 2, function(x) all(is.na(x))))]
+    # remove these columns
+    dt <- dt[, .SD, .SDcols = -na_cols]
+    # convert to a matrix and remove column names
+    mat <- as.matrix(dt)
+    colnames(mat) <- NULL
+    # return the matrix itself
+    return(mat)
+}
 
 
 # ==============================================================================
@@ -24,23 +45,36 @@ BENIGN_SAMPLES <- metadata[Type == "Benign", SampleID]
 # Analysis
 # ==============================================================================
 # loop over each chromosome
-for (chrom in rev(CHRS)) {
-    tumour_mats <- lapply(
-        TUMOUR_SAMPLES,
-        function(id) {
-            as.matrix(fread(
-                cmd = paste0("zcat TMP/", id, ".300000000.res_40000bp.", chrom, ".noheader.mtx.gz"),
-                sep = "\t"
-            ))
-        }
-    )
-    names(tumour_mats) <- TUMOUR_SAMPLES
-    for (s in tumour_mats) {print(head(tumour_mats[[s]]))}
-    readline()
+chrom_mats <- list(
+    "tumour" = list(),
+    "benign" = list()
+)
+
+for (chrom in CHRS) {
+    cat(chrom, "\n")
+    tumour_mats <- lapply(TUMOUR_SAMPLES[1:2], read_contacts, chrom)
+    # calculate the mean contact matrix from all the tumour matrices
+    chrom_mats[["tumour"]][[chrom]] <- Reduce("+", tumour_mats) / length(tumour_mats)
+    benign_mats <- lapply(BENIGN_SAMPLES[1:2], read_contacts, chrom)
+    # calculate the mean contact matrix from all the tumour matrices
+    chrom_mats[["benign"]][[chrom]] <- Reduce("+", benign_mats) / length(benign_mats)
 }
 
 
 # ==============================================================================
 # Save data
 # ==============================================================================
+for (chrom in CHRS) {
+    cat(chrom, "\n")
+    for (i in c("tumour", "benign")) {
+        cat("\t", i, "\n")
+        dt <- as.data.table(chrom_mats[[i]][[chrom]])
+        fwrite(
+            dt,
+            paste0("TMP/", i, ".300000000.res_40000bp.", chrom, ".mtx"),
+            sep = "\t",
+            col.names = FALSE
+        )
+    }
+}
 
