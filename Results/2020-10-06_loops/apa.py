04ad1d435de5391e2545a95ee45345b0f2b98e67
James Hawley <jamesrichardhawley@gmail.com>
2020-10-15 16:30:08 -0400

    Fixes to apa.py

--- Results/2020-10-06_loops/apa.py
+++ Results/2020-10-06_loops/apa.py
@@ -6,17 +6,16 @@
 # Description: Aggregate peak analysis for differential tumour-vs-benign loop calls
 # Author: James Hawley
 
-import argparse
 import os.path as path
 import numpy as np
-import datatable as dt
-from datatable import f, by, update
 from cooler import Cooler
-import matplotlib.pyplot as plt
 import scipy.stats as stats
 from tqdm import tqdm
 import negspy.coordinates as nc
 import logging
+from cooltools import snipping
+import pandas as pd
+import bioframe
 
 logging.getLogger().setLevel(logging.INFO)
 
@@ -33,34 +32,37 @@ DIR = {
 # resolution for contact matrices
 RES = 5000
 
-# +/- number of bins for aggregate peak analysis
-SHIFT_IDX = int(RES / 1000)
+# +/- number of bps for aggregate peak analysis
+SHIFT_SIZE = 25000
 
 # genome coordinates
 hg38 = nc.get_chrominfo("hg38")
 
-# ==============================================================================
-# Functions
-# ==============================================================================
+CHROM_SIZES = bioframe.fetch_chromsizes("hg38")
+CHRS = list(CHROM_SIZES.index)
 
 # ==============================================================================
 # Data
 # ==============================================================================
 logging.info("Loading data")
 # load sample metadata
-metadata = dt.fread("config.tsv", sep="\t")
+metadata = pd.read_csv("config.tsv", sep="\t")
 # remove samples that shouldn't be included in analyses
-metadata = metadata[f.Include == "Yes", :]
+metadata = metadata.loc[metadata.Include == "Yes", :]
 # extract sample IDs
-SAMPLES = metadata["SampleID"].to_list()[0]
-TUMOUR_SAMPLES = metadata[f.Type == "Malignant", f.SampleID].to_list()[0]
-BENIGN_SAMPLES = metadata[f.Type == "Benign", f.SampleID].to_list()[0]
+SAMPLES = metadata["SampleID"].tolist()
+TUMOUR_SAMPLES = metadata.loc[metadata.Type == "Malignant", "SampleID"].tolist()
+BENIGN_SAMPLES = metadata.loc[metadata.Type == "Benign", "SampleID"].tolist()
 
 # load loop calls
 loops = {
-    "shared": dt.fread(path.join(DIR["loops"], "shared-loops.tsv"), sep="\t"),
-    "tumour": dt.fread(path.join(DIR["loops"], "tumour-specific-loops.tsv"), sep="\t"),
-    "benign": dt.fread(path.join(DIR["loops"], "benign-specific-loops.tsv"), sep="\t"),
+    "shared": pd.read_csv(path.join(DIR["loops"], "shared-loops.tsv"), sep="\t"),
+    "tumour": pd.read_csv(
+        path.join(DIR["loops"], "tumour-specific-loops.tsv"), sep="\t"
+    ),
+    "benign": pd.read_csv(
+        path.join(DIR["loops"], "benign-specific-loops.tsv"), sep="\t"
+    ),
 }
 
 # contact matrices
@@ -69,49 +71,77 @@ mtx = {
     for s in SAMPLES
 }
 
+# expected matrix values
+expected_mtx = {
+    s: pd.read_csv(path.join(DIR["contacts"], s + ".res_10000bp.exp.cis.tsv"), sep="\t")
+    for s in SAMPLES
+}
+
 # ==============================================================================
 # Analysis
 # ==============================================================================
 # combine coordinates into UCSC-like strings for getting region extents
 logging.info("Calculating loop matrix indices")
-for (t, l) in loops.items():
-    for i in range(l.nrows):
-        # find matrix row/col IDs
-        x_reg = (l[i, "chr_x"], l[i, "start_x"], l[i, "end_x"])
-        y_reg = (l[i, "chr_y"], l[i, "start_y"], l[i, "end_y"])
-        x_centre = int(
-            sum(
-                [
-                    nc.chr_pos_to_genome_pos(x_reg[0], x_reg[1], hg38),
-                    nc.chr_pos_to_genome_pos(x_reg[0], x_reg[2], hg38),
-                ]
-            )
-            / (2 * RES)
-        )
-        y_centre = int(
-            sum(
-                [
-                    nc.chr_pos_to_genome_pos(y_reg[0], y_reg[1], hg38),
-                    nc.chr_pos_to_genome_pos(y_reg[0], y_reg[2], hg38),
-                ]
-            )
-            / (2 * RES)
-        )
-        l[i, "x_left"] = x_centre - SHIFT_IDX - 1
-        l[i, "x_right"] = x_centre + SHIFT_IDX
-        l[i, "y_left"] = y_centre - SHIFT_IDX - 1
-        l[i, "y_right"] = y_centre + SHIFT_IDX
+
+# chromosomes and their sizes
+supports = [(chrom, 0, CHROM_SIZES[chrom]) for chrom in CHRS]
+
+# placeholder for all loop positions and matrix indices
+windows = {
+    "shared": pd.DataFrame(),
+    "tumour": pd.DataFrame(),
+    "benign": pd.DataFrame(),
+}
+
+for (loop_type, typed_loops) in loops.items():
+    # use snipping module to get chromosome locations around each loop anchor
+    windows_x = snipping.make_bin_aligned_windows(
+        binsize=RES,
+        chroms=typed_loops["chr_x"],
+        centers_bp=(typed_loops["start_x"] + typed_loops["end_x"])
+        // 2,  # integer division
+        flank_bp=SHIFT_SIZE,
+    )
+    windows_y = snipping.make_bin_aligned_windows(
+        binsize=RES,
+        chroms=typed_loops["chr_y"],
+        centers_bp=(typed_loops["start_y"] + typed_loops["end_y"])
+        // 2,  # integer division
+        flank_bp=SHIFT_SIZE,
+    )
+    # merge the two pairs of windows together
+    windows_merged = pd.merge(
+        left=windows_x,
+        right=windows_y,
+        left_index=True,
+        right_index=True,
+        suffixes=(
+            "1",
+            "2",
+        ),  # these suffixes are required, assign_regions doesn't work properly if not '1' and '2'
+    )
+    # assign chromosome regions to the low/high indices
+    windows[loop_type] = snipping.assign_regions(windows_merged, supports)
+    windows[loop_type] = windows[loop_type].dropna()
 
 logging.info("Aggregating matrices")
-for (t, l) in loops.items():
-    # set placeholder matrices
-    local_loop_matrices = {
-        "tumour": [np.zeros((2 * SHIFT_IDX + 1, 2 * SHIFT_IDX + 1))] * l.nrows,
-        "benign": [np.zeros((2 * SHIFT_IDX + 1, 2 * SHIFT_IDX + 1))] * l.nrows,
+# calculate obs/exp matrix for each sample
+snipper = {
+    s: snipping.ObsExpSnipper(mtx[s], expected_mtx[s]) for s in SAMPLES
+}
+for (loop_type, typed_loops) in loops.items():
+    # taken from https://cooltools.readthedocs.io/en/latest/notebooks/06_snipping-pileups.html
+    stack = {
+        s: snipping.pileup(windows[loop_type], snipper[s].select, snipper[s].snip) for s in SAMPLES
     }
+    print(stack)
+    piles = np.nanmean(stack, axis=2)
+    print(piles)
+    input()
+
     # store local matrices in the regions of interest
     for i in tqdm(range(l.nrows)):
-        local_loop_matrices["tumour"][i] = np.nanmean(
+        local_loop_matrices["tumour"][i] = np.nanconda actmean(
             [
                 mtx[s].matrix()[
                     l[i, "x_left"] : l[i, "x_right"], l[i, "y_left"] : l[i, "y_right"]
