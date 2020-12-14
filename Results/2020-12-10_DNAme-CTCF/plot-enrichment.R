846cea3fc5a930af6f958eac93283c196c1eab1b
James Hawley <jamesrichardhawley@gmail.com>
2020-12-14 18:12:33 -0500

    Updated prostate DMRs within CTCF binding site enrichment plots
    Conclusion of not looking at hypermethylation globally, still stands.
    There is an "enrichment" of hyper-DMRs within CTCF binding sites compared to a global background, but this isn't uniquely sensitive in just observed areas.
    This local sensitivity is, however, observed, for the demethylated sites.
    Which may indicated looking at a loss of methylation and stronger insulating effect may be more reasonable

--- Results/2020-12-10_DNAme-CTCF/plot-enrichment.R
+++ Results/2020-12-10_DNAme-CTCF/plot-enrichment.R
@@ -24,50 +24,105 @@ suppressMessages(library("ggplot2"))
 # Data
 # ==============================================================================
 loginfo("Loading data")
-perm_test <- readRDS("Overlaps/permutation-tests.rds")
-lz <- readRDS("Overlaps/local-dependency.rds")
-perm_test_data <- fread("Overlaps/permutations.tsv")
+perm_test_all <- readRDS("Overlaps/all-dmrs.permutation-tests.rds")
+perm_summ_all <- fread("Overlaps/all-dmrs.permutations.tsv")
+lz_all <- readRDS("Overlaps/all-dmrs.local-dependency.rds")
+lz_all_dt <- fread("Overlaps/all-dmrs.local-dependency.tsv")
 
-CELL_LINES <- names(perm_test)
+perm_test_up <- readRDS("Overlaps/pca-hyper-dmrs.permutation-tests.rds")
+perm_summ_up <- fread("Overlaps/pca-hyper-dmrs.permutations.tsv")
+lz_up <- readRDS("Overlaps/pca-hyper-dmrs.local-dependency.rds")
+lz_up_dt <- fread("Overlaps/pca-hyper-dmrs.local-dependency.tsv")
 
-perm_tests <- rbindlist(lapply(
+perm_test_dn <- readRDS("Overlaps/pca-hypo-dmrs.permutation-tests.rds")
+perm_summ_dn <- fread("Overlaps/pca-hypo-dmrs.permutations.tsv")
+lz_dn <- readRDS("Overlaps/pca-hypo-dmrs.local-dependency.rds")
+lz_dn_dt <- fread("Overlaps/pca-hypo-dmrs.local-dependency.tsv")
+
+CELL_LINES <- names(perm_test_all)
+
+perm_test_data_all <- rbindlist(lapply(
     CELL_LINES,
     function(cl) {
         data.table(
             Cell_Line = cl,
-            Iteration = 1:perm_test[[cl]]$numOverlaps$ntimes,
-            Permuted = perm_test[[cl]]$numOverlaps$permuted,
-            log2Perm_Obs = log2(perm_test[[cl]]$numOverlaps$permuted / perm_test[[cl]]$numOverlaps$observed)
+            Iteration = 1:perm_test_all[[cl]]$numOverlaps$ntimes,
+            Permuted = perm_test_all[[cl]]$numOverlaps$permuted,
+            log2Perm_Obs = log2(perm_test_all[[cl]]$numOverlaps$permuted / perm_test_all[[cl]]$numOverlaps$observed)
         )
     }
 ))
 
-obs_tests <- rbindlist(lapply(
+obs_all <- rbindlist(lapply(
     CELL_LINES,
     function(cl) {
         data.table(
             Cell_Line = cl,
-            Observed = perm_test[[cl]]$numOverlaps$observed
+            Observed = perm_test_all[[cl]]$numOverlaps$observed
         )
     }
 ))
 
-lz_dt <- fread("Overlaps/local-dependency.tsv")
+perm_test_data_up <- rbindlist(lapply(
+    CELL_LINES,
+    function(cl) {
+        data.table(
+            Cell_Line = cl,
+            Iteration = 1:perm_test_up[[cl]]$numOverlaps$ntimes,
+            Permuted = perm_test_up[[cl]]$numOverlaps$permuted,
+            log2Perm_Obs = log2(perm_test_up[[cl]]$numOverlaps$permuted / perm_test_up[[cl]]$numOverlaps$observed)
+        )
+    }
+))
+
+obs_up <- rbindlist(lapply(
+    CELL_LINES,
+    function(cl) {
+        data.table(
+            Cell_Line = cl,
+            Observed = perm_test_up[[cl]]$numOverlaps$observed
+        )
+    }
+))
+
+
+perm_test_data_dn <- rbindlist(lapply(
+    CELL_LINES,
+    function(cl) {
+        data.table(
+            Cell_Line = cl,
+            Iteration = 1:perm_test_dn[[cl]]$numOverlaps$ntimes,
+            Permuted = perm_test_dn[[cl]]$numOverlaps$permuted,
+            log2Perm_Obs = log2(perm_test_dn[[cl]]$numOverlaps$permuted / perm_test_dn[[cl]]$numOverlaps$observed)
+        )
+    }
+))
+
+obs_dn <- rbindlist(lapply(
+    CELL_LINES,
+    function(cl) {
+        data.table(
+            Cell_Line = cl,
+            Observed = perm_test_dn[[cl]]$numOverlaps$observed
+        )
+    }
+))
+
 
 # ==============================================================================
 # Plots
 # ==============================================================================
 loginfo("Plotting figures")
 gg <- (
-    ggplot(data = perm_tests)
+    ggplot(data = perm_test_data_all)
     + geom_histogram(aes(x = Permuted), bins = 100)
     + geom_vline(
-        data = obs_tests,
+        data = obs_all,
         mapping = aes(xintercept = Observed),
         linetype = "dashed"
     )
     + geom_text(
-        data = obs_tests,
+        data = obs_all,
         mapping = aes(x = Observed, y = 100, label = "Observed"),
         angle = 90,
         hjust = 0.5,
@@ -78,21 +133,101 @@ gg <- (
     + facet_wrap(~ Cell_Line)
 )
 ggsave(
-    "Plots/DMR-CTCF.permutations.hist.png",
+    "Plots/DMR-CTCF.all-dmrs.permutations.hist.png",
     width = 16,
     height = 8,
     units = "cm"
 )
 
 gg <- (
-    ggplot(data = lz_dt)
+    ggplot(data = lz_all_dt)
     + geom_line(aes(x = Shift, y = z))
     + labs(x = "Local shift (bp)", y = "Shifted Z-score")
     + theme_minimal()
     + facet_wrap(~ Cell_Line)
 )
 ggsave(
-    "Plots/DMR-CTCF.permutations.shifted-z.png",
+    "Plots/DMR-CTCF.all-dmrs.permutations.shifted-z.png",
+    height = 12,
+    width = 20,
+    units = "cm"
+)
+
+gg <- (
+    ggplot(data = perm_test_data_up)
+    + geom_histogram(aes(x = Permuted), bins = 100)
+    + geom_vline(
+        data = obs_up,
+        mapping = aes(xintercept = Observed),
+        linetype = "dashed"
+    )
+    + geom_text(
+        data = obs_up,
+        mapping = aes(x = Observed, y = 100, label = "Observed"),
+        angle = 90,
+        hjust = 0.5,
+        vjust = -0.5
+    )
+    + labs(x = "DMR-CTCF Intersections", y = "Frequency")
+    + theme_minimal()
+    + facet_wrap(~ Cell_Line)
+)
+ggsave(
+    "Plots/DMR-CTCF.pca-hyper-dmrs.permutations.hist.png",
+    width = 16,
+    height = 8,
+    units = "cm"
+)
+
+gg <- (
+    ggplot(data = lz_up_dt)
+    + geom_line(aes(x = Shift, y = z))
+    + labs(x = "Local shift (bp)", y = "Shifted Z-score")
+    + theme_minimal()
+    + facet_wrap(~ Cell_Line)
+)
+ggsave(
+    "Plots/DMR-CTCF.pca-hyper-dmrs.permutations.shifted-z.png",
+    height = 12,
+    width = 20,
+    units = "cm"
+)
+
+gg <- (
+    ggplot(data = perm_test_data_dn)
+    + geom_histogram(aes(x = Permuted), bins = 100)
+    + geom_vline(
+        data = obs_dn,
+        mapping = aes(xintercept = Observed),
+        linetype = "dashed"
+    )
+    + geom_text(
+        data = obs_dn,
+        mapping = aes(x = Observed, y = 100, label = "Observed"),
+        angle = 90,
+        hjust = 0.5,
+        vjust = -0.5
+    )
+    + labs(x = "DMR-CTCF Intersections", y = "Frequency")
+    + theme_minimal()
+    + facet_wrap(~ Cell_Line)
+)
+ggsave(
+    "Plots/DMR-CTCF.pca-hypo-dmrs.permutations.hist.png",
+    width = 16,
+    height = 8,
+    units = "cm"
+)
+
+gg <- (
+    ggplot(data = lz_dn_dt)
+    + geom_line(aes(x = Shift, y = z))
+    + labs(x = "Local shift (bp)", y = "Shifted Z-score")
+    + theme_minimal()
+    + facet_wrap(~ Cell_Line)
+)
+ggsave(
+    "Plots/DMR-CTCF.pca-hypo-dmrs.permutations.shifted-z.png",
     height = 12,
     width = 20,
     units = "cm"
