aa4c8875b0c9fe63b9bd0e5339724d39431eac50
James Hawley <james@jrhawley.ca>
2021-03-04 11:23:50 -0500

    Adding GC content phasing for compartments

--- Data/Processed/2019-06-18_PCa-LowC-sequencing/get_a_phase.py
+++ Data/Processed/2019-06-18_PCa-LowC-sequencing/get_a_phase.py
@@ -1,38 +1,44 @@
 # ==============================================================================
 # Environment
 # ==============================================================================
-import numpy as np
+print("Loading packages")
 import pandas as pd
 import bioframe
-import cooltools
 import cooler
 
 # ==============================================================================
 # Data
 # ==============================================================================
+print("Loading reference genome")
 # reference genome
 hg38 = bioframe.fetch_chromsizes("hg38")
 
+# load hg38 reference sequence
+fasta_records = bioframe.load_fasta(
+    "/mnt/work1/data/genomes/human/hg38/iGenomes/Sequence/WholeGenomeFasta/genome.fa"
+)
+
+print("Binning the genome")
 # compartment calling resolution
 res = 40000
 
 # create bins of the reference genome
 bins = cooler.binnify(hg38, res)
 
-# load hg38 reference sequence
-fasta_records = bioframe.load_fasta("data/hg38.fa")
-
 # ==============================================================================
 # Analysis
 # ==============================================================================
+print("Calculating GC content")
 # calculate GC content in each bin
 bins["GC"] = bioframe.tools.frac_gc(bins, fasta_records)
 
 # ==============================================================================
 # Save data
 # ==============================================================================
+print("Saving bedGraph")
 bins.to_csv(
-	"gc-content-phase.bedGraph",
-	sep="\t",
-	names=True,
+    "gc-content-phase.bedGraph",
+    sep="\t",
+    index=False,
+    header=True,
 )
